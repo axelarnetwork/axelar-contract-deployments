27e480f3da8d9c039830b5189f1d8b897b98ccd9
Milap Sheth <milap@axelar.network>
2023-05-22 03:36:06 -0400

    feat: add hardhat config and evm utils

---
+++ hardhat.config.js
@@ -0,0 +1,46 @@
+require('@nomicfoundation/hardhat-toolbox');
+
+const fs = require('fs');
+const env = process.env.NETWORK || 'testnet';
+const { importNetworks } = require('./evm/utils');
+const chains = require(`./info/${env}.json`);
+const keys = fs.existsSync('./info/keys.json') ? require('./info/keys.json') : {}; // Load keys if they exist
+const { networks, etherscan } = importNetworks(chains, keys);
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    solidity: {
+        version: '0.8.9',
+        settings: {
+            evmVersion: process.env.EVM_VERSION || 'london',
+            optimizer: {
+                enabled: true,
+                runs: 1000,
+                details: {
+                    peephole: true,
+                    inliner: true,
+                    jumpdestRemover: true,
+                    orderLiterals: true,
+                    deduplicate: true,
+                    cse: true,
+                    constantOptimizer: true,
+                    yul: true,
+                    yulDetails: {
+                        stackAllocation: true,
+                    },
+                },
+            },
+        },
+    },
+    defaultNetwork: 'hardhat',
+    networks,
+    etherscan,
+    mocha: {
+        timeout: 1000000,
+    },
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== '',
+    },
+};
