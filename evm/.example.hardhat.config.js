cd9300a9dee4c447c6b4102d0821699819848b62
Milap Sheth <milap@axelar.network>
2023-05-22 19:29:59 -0400

    refactor: add more visibility to exports

--- evm/.example.hardhat.config.js
+++ evm/.example.hardhat.config.js
@@ -3,23 +3,23 @@ require('@nomicfoundation/hardhat-toolbox');
 
 const fs = require('fs');
 const env = process.env.ENV || 'testnet';
-const { importNetworks } = require('@axelar-network/axelar-gmp-sdk-solidity');
-const chains = require(`@axelar-network/axelar-cgp-solidity/info/${env}.json`);
-const keys = fs.existsSync('keys.json') ? require('keys.json') : {}; // Load keys if they exist
+const { importNetworks } = require('@axelar-network/axelar-contract-deployments');
+const chains = require(`@axelar-network/axelar-contract-deployments/info/${env}.json`);
+const keys = fs.existsSync('keys.json') ? require('keys.json') : undefined; // Load keys if they exist
 const { networks, etherscan } = importNetworks(chains, keys);
 
 module.exports = {
-  solidity: {
-    version: '0.8.9',
-    settings: {},
-  },
-  defaultNetwork: 'hardhat',
-  networks: networks,
-  etherscan: etherscan,
-  mocha: {
-    timeout: 1000000, // Keep a higher timeout since tests on live networks can take much longer
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS ? true : false,
-  },
+    solidity: {
+        version: '0.8.9',
+        settings: {},
+    },
+    defaultNetwork: 'hardhat',
+    networks: networks,
+    etherscan: etherscan,
+    mocha: {
+        timeout: 1000000, // Keep a higher timeout since tests on live networks can take much longer
+    },
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== '',
+    },
 };
