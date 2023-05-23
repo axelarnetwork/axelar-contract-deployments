b62b6dc94b57e8485fd5f93ad9d41c550d9ad3da
Milap Sheth <milap@axelar.network>
2023-05-23 07:42:19 -0400

    revert: "chore: remove evm/index.js"
    This reverts commit a80c991c8541639a5a7a0a12a4ef324a6524e650.

---
+++ evm/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const { printObj, writeJSON, importNetworks, verifyContract } = require('./utils');
+
+module.exports = {
+    printObj,
+    writeJSON,
+    importNetworks,
+    verifyContract,
+};
