e21c5e5e34cf893ae77b2c4b66c071773820c110
Dean Amiel <damiel@deans-mbp.lan>
2023-05-31 03:39:16 -0400

    manually import artifacts to ensure correct bytecode

--- contracts/import.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.9;
-
-import '@axelar-network/axelar-cgp-solidity/contracts/gas-service/AxelarGasService.sol';
-import '@axelar-network/axelar-cgp-solidity/contracts/gas-service/AxelarGasServiceProxy.sol';
-import '@axelar-network/axelar-cgp-solidity/contracts/deposit-service/AxelarDepositService.sol';
-import '@axelar-network/axelar-cgp-solidity/contracts/deposit-service/AxelarDepositServiceProxy.sol';
