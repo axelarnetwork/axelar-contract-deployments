641222c22032e6d72ff4a210f1f88a596b75efa9
Ayush Tiwari <ayush@axelar.network>
2023-09-15 13:58:14 +0530

    add util functions for broadcasting signed txscript

--- evm/offline-sign-utils.js
+++ evm/offline-sign-utils.js
@@ -2,12 +2,11 @@
 
 const {
     BigNumber,
-    utils: { isAddress, serializeTransaction },
+    utils: { isAddress },
 } = require('ethers');
 const { LedgerSigner } = require('@ethersproject/hardware-wallets');
-const { printObj, isNumber, printError } = require('./utils');
+const { printError } = require('./utils');
 const fs = require('fs');
-
 
 // function to create a ledgerSigner type wallet object
 function getLedgerWallet(provider, path) {
@@ -15,7 +14,7 @@ function getLedgerWallet(provider, path) {
     if (provider === undefined || provider === null) {
         throw new Error('Provider is not provided while creating a ledger wallet');
     }
-    provider = provider;
+
     const type = 'hid';
     path = path || "m/44'/60'/0'/0/0";
     return new LedgerSigner(provider, type, path);
@@ -27,22 +26,31 @@ async function ledgerSign(gasLimit, gasPrice, nonce, chain, wallet, to, amount, 
     if (!chain) {
         throw new Error('Chain is missing in the function arguments');
     }
+
     tx.chainId = chain.chainId;
+
     if (!gasLimit) {
         throw new Error('Gas limit is missing in the function arguments');
     }
+
     tx.gasLimit = gasLimit;
+
     if (!gasPrice) {
         throw new Error('Gas price is missing in the function arguments');
     }
+
     tx.gasPrice = gasPrice;
+
     if (!nonce) {
         throw new Error('Nonce is missing in the function arguments');
     }
+
     tx.nonce = nonce;
+
     if (!wallet) {
         throw new Error('Wallet is missing/not provided correctly in function arguments');
     }
+
     if (!to || !isAddress(to)) {
         throw new Error('Target address is missing/not provided as valid address for the tx in funciton arguments');
     }
@@ -54,11 +62,12 @@ async function ledgerSign(gasLimit, gasPrice, nonce, chain, wallet, to, amount, 
         if (to.toLowerCase() !== contract.address.toLowerCase()) {
             throw new Error('Contract address do not matches the to address provided in function arguments');
         }
+
         if (!functionName) {
             throw new Error('Function name is missing in the funciton arguments');
         }
-        const data = contract.interface.encodeFunctionData(functionName, args);
 
+        const data = contract.interface.encodeFunctionData(functionName, args);
         tx.data = data || undefined;
     }
 
@@ -72,60 +81,25 @@ async function ledgerSign(gasLimit, gasPrice, nonce, chain, wallet, to, amount, 
         value: tx.value || undefined,
     };
 
-
     const signedTx = await wallet.signTransaction(baseTx);
     return [baseTx, signedTx];
 }
 
-async function fetchExisitingTransactions(directoryPath, fileName) {
-    // Read the existing transactions from the file or create a new array if the file doesn't exist
-    let transactions = [];
-    const filePath = getFilePath(directoryPath, fileName);
-    try {
-        const existingData = getFileData(filePath);
-        transactions = JSON.parse(existingData).transactions;
-    } catch (error) {
-        printError("File doesn't exist yet, that's fine");
-    }
-    return transactions;
-}
-
 function getFilePath(directoryPath, fileName) {
-    if(!directoryPath) {
-        throw new Error("Directory path is missing in the function arguments");
+    if (!directoryPath) {
+        throw new Error('Directory path is missing in the function arguments');
     }
-    if(!fileName) {
-        throw new Error("File name is missing in the function arguments");
+
+    if (!fileName) {
+        throw new Error('File name is missing in the function arguments');
     }
+
     if (!fs.existsSync(directoryPath)) {
         fs.mkdirSync(directoryPath);
     }
+
     const filePath = directoryPath + '/' + fileName + '.json';
     return filePath;
-}
-
-function updateTransactions(transactions, msg, signedTransaction) {
-    transactions.push({ msg: msg, signedTransaction: signedTransaction });
-    return transactions;
-}
-
-async function storeTransactionsData(directoryPath, fileName, msg, signedTransaction) {
-    let transactions = await fetchExisitingTransactions(directoryPath, fileName);
-    transactions = updateTransactions(transactions, msg, signedTransaction);
-    directoryPath = directoryPath || './tx';
-    if (!fs.existsSync(directoryPath)) {
-        fs.mkdirSync(directoryPath);
-    }
-    fileName = fileName || 'signed_transactions.txt';
-    const filePath = directoryPath + '/' + fileName;
-    const data = JSON.stringify({ transactions }, null, 2);
-    fs.writeFileSync(filePath, data, (err) => {
-        if (err) {
-            printError(err);
-            return;
-        }
-        print(`Data has been successfully stored in the ${filePath} file.`);
-    });
 }
 
 async function sendTx(tx, provider) {
@@ -140,7 +114,8 @@ async function updateSignersData(directoryPath, fileName, signersData) {
             printError(err);
             return;
         }
-        print(`Data has been successfully stored in the ${filePath} file.`);
+
+        printError(`Data has been successfully stored in the ${filePath} file.`);
     });
 }
 
@@ -153,60 +128,67 @@ async function getLatestNonceAndUpdateData(directoryPath, fileName, wallet) {
     try {
         const provider = wallet.provider;
         const signerAddress = await wallet.getAddress();
-        let signersData = getAllSignersData(directoryPath, fileName);
+        const signersData = await getAllSignersData(directoryPath, fileName);
         let signerData = signersData[signerAddress];
-        const nonceFromData = getLatestNonceFromData(signerData); 
+        const nonceFromData = getLatestNonceFromData(signerData);
         let nonce = await getNonceFromProvider(provider, signerAddress);
-        if(nonce > nonceFromData) {
+
+        if (nonce > nonceFromData) {
             signerData = updateTxNonceAndStatus(signerData, nonce);
             signersData[signerAddress] = signerData;
             await updateSignersData(directoryPath, fileName, signersData);
-        }
-        else {
+        } else {
             nonce = nonceFromData + 1;
         }
-        return nonce;
 
-    } catch(error) {
+        return nonce;
+    } catch (error) {
         printError(error.message);
     }
 }
 
 function updateTxNonceAndStatus(signerData, nonce) {
-    if(signerData) {
-        for(const transaction of signerData) {
-            if(nonce > transaction.nonce && (transaction.status === "PENDING" || transaction.status === "BROADCASTED")) {
-                transaction.status = "FAILED";
+    if (signerData) {
+        for (const transaction of signerData) {
+            if (nonce > transaction.nonce && (transaction.status === 'PENDING' || transaction.status === 'BROADCASTED')) {
+                transaction.status = 'FAILED';
                 transaction.error = `Transaction nonce value of ${transaction.nonce} is less than the required signer nonce value of ${nonce}`;
             }
         }
     }
-    return signerData
+
+    return signerData;
 }
 
 function getLatestNonceFromData(signerData) {
-    if(signerData) {
+    if (signerData) {
         const length = signerData.length;
         return parseInt(signerData[length - 1].nonce);
     }
+
     return 0;
 }
 
 async function getAllSignersData(directoryPath, fileName) {
     const signersData = {};
+
     try {
         const filePath = getFilePath(directoryPath, fileName);
         // Read the content of the file
         const data = getFileData(filePath);
-        if(data) {
+
+        if (data) {
             const jsonData = JSON.parse(data);
-            if(!isValidJSON(jsonData)) {
+
+            if (!isValidJSON(jsonData)) {
                 return signersData;
             }
+
             return jsonData;
         }
+
         return signersData;
-    } catch(error) {
+    } catch (error) {
         printError(error.message);
     }
 }
@@ -215,61 +197,61 @@ function getFileData(filePath) {
     try {
         if (!fs.existsSync(filePath)) {
             // File does not exist, create it
-            fs.writeFileSync(filePath, {});
+            fs.writeFileSync(filePath, JSON.stringify({}));
             return undefined;
         }
         // Read the content of the file
+
         const data = fs.readFileSync(filePath);
         return data;
-    } catch(error) {
+    } catch (error) {
         printError(error.message);
     }
 }
 
 async function getSignerData(directoryPath, fileName, signerAddress) {
     let signerData = [];
-    console.log(fileName);
+
     try {
         const filePath = getFilePath(directoryPath, fileName);
         // Read the content of the file
         const data = getFileData(filePath);
-        console.log(data);
-        if(data) {
+
+        if (data) {
             const jsonData = JSON.parse(data);
-            console.log(jsonData);
-            if(!isValidJSON(jsonData)) {
+
+            if (!isValidJSON(jsonData)) {
                 return signerData;
             }
             // Access the transactions array from the JSON object
-            if(signerAddress in jsonData) {
-                console.log("line 245");
+
+            if (signerAddress in jsonData) {
                 signerData = jsonData[signerAddress];
             }
         }
-        return signerData;
 
-    } catch(error) {
+        return signerData;
+    } catch (error) {
         printError(error.message);
     }
 }
 
 function isValidJSON(obj) {
     if (obj === undefined || obj === null) {
-      return false;
+        return false;
     }
-  
+
     if (Object.keys(obj).length === 0 && obj.constructor === Object) {
-      return false;
+        return false;
     }
-  
+
     return true;
-  }
+}
 
 module.exports = {
     getLedgerWallet,
     ledgerSign,
     sendTx,
-    storeTransactionsData,
     getFilePath,
     updateTxNonceAndStatus,
     getNonceFromProvider,
@@ -277,5 +259,6 @@ module.exports = {
     getAllSignersData,
     getSignerData,
     updateSignersData,
-    getLatestNonceAndUpdateData
+    getLatestNonceAndUpdateData,
+    isValidJSON,
 };
