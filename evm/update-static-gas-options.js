350fcd4b1c5b880b5dd99eea12968f6189965f46
Ayush Tiwari <ayush@axelar.network>
2023-09-20 19:06:11 +0530

    update logic to only update gasLimit if not already set

--- evm/update-static-gas-options.js
+++ evm/update-static-gas-options.js
@@ -9,11 +9,11 @@ const {
 } = ethers;
 const readlineSync = require('readline-sync');
 
-const { printError, printInfo, printObj, saveConfig } = require('./utils');
+const { printError, printInfo, printObj, saveConfig, loadConfig } = require('./utils');
 
 async function updateStaticGasOptions(chain, options, filePath) {
     const { rpcUrl, yes } = options;
-    const provider = getDefaultProvider(rpcUrl);
+    const provider = rpcUrl ? getDefaultProvider(rpcUrl) : getDefaultProvider(chain.rpc);
     const network = await provider.getNetwork();
 
     if (!yes) {
@@ -27,10 +27,8 @@ async function updateStaticGasOptions(chain, options, filePath) {
 
     try {
         const gasPrice = parseUnits((await provider.getGasPrice()).toString(), 'gwei') * 5;
-        const staticGasOptions = {};
-        staticGasOptions.gasLimit = 130000;
+        chain.staticGasOptions.gasLimit = chain.staticGasOptions.gasLimit || 3e6;
         staticGasOptions.gasPrice = gasPrice;
-        chain.staticGasOptions = staticGasOptions;
         printInfo(`GasOptions updated succesfully and stored in config file ${filePath}`);
     } catch (error) {
         printError(`GasOptions updation failed with error: ${error.message}`);
@@ -42,10 +40,7 @@ async function updateStaticGasOptions(chain, options, filePath) {
 
 async function main(options) {
     const { env, chainNames } = options;
-
-    const filePath = `${__dirname}/../axelar-chains-config/info/${env === 'local' ? 'testnet' : env}.json`;
-    const config = require(filePath);
-
+    const config = loadConfig(env);
     const chains = chainNames.split(',').map((str) => str.trim());
 
     for (const chainName of chains) {
@@ -64,7 +59,7 @@ async function main(options) {
 
 const program = new Command();
 
-program.name('Update-GasOptions').description('Update gasOptions to be used in offline signing');
+program.name('update-static-gas-options').description('Update staticGasOptions to be used when offline signing');
 
 program.addOption(
     new Option('-e, --env <env>', 'environment')
@@ -74,7 +69,7 @@ program.addOption(
         .env('ENV'),
 );
 program.addOption(new Option('-n, --chainNames <chainNames>', 'chain names').makeOptionMandatory(true));
-program.addOption(new Option('-r, --rpcUrl <rpcUrl>', 'The rpc url for creating a provider to fetch gasOptions').makeOptionMandatory(true));
+program.addOption(new Option('-r, --rpcUrl <rpcUrl>', 'The rpc url for creating a provider to fetch gasOptions'));
 program.addOption(new Option('-y, --yes', 'skip prompts'));
 
 program.action((options) => {
