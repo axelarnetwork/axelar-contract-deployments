df1c70f75ccb9a7e710393b1e3216a6246269b08
Milap Sheth <milap@axelar.network>
2023-10-18 05:52:01 -0400

    chore: add static gas options

--- evm/update-static-gas-options.js
+++ evm/update-static-gas-options.js
@@ -13,21 +13,51 @@ const { printInfo, mainProcessor, prompt } = require('./utils');
 const defaultGasLimit = 3e6;
 const gasPriceMultiplier = 5;
 
-async function processCommand(config, chain, options) {
-    const { rpc, yes } = options;
+const minGasPrices = {
+    mainnet: {
+        ethereum: 150,
+        moonbeam: 500,
+        avalanche: 150,
+        polygon: 350,
+        fantom: 1000,
+        binance: 30,
+        arbitrum: 2,
+        celo: 100,
+        kava: 50,
+        optimism: 10,
+        filecoin: 1,
+        base: 10,
+        linea: 10,
+        mantle: 25,
+        scroll: 25,
+    },
+    testnet: {
+        mantle: 1,
+    },
+};
+
+async function processCommand(_, chain, options) {
+    const { env, rpc, yes } = options;
     const provider = rpc ? getDefaultProvider(rpc) : getDefaultProvider(chain.rpc);
 
     if (prompt(`Proceed with the static gasOption update on ${chalk.green(chain.name)}`, yes)) {
         return;
     }
 
-    const gasPrice = parseUnits((await provider.getGasPrice()).toString(), 'wei') * gasPriceMultiplier;
+    const gasPriceWei = await provider.getGasPrice();
+    printInfo(`${chain.name} gas price`, `${gasPriceWei / 1e9} gwei`);
+
+    let gasPrice = parseUnits(gasPriceWei.toString(), 'wei') * gasPriceMultiplier;
 
     if (!(chain.staticGasOptions && chain.staticGasOptions.gasLimit !== undefined)) {
         chain.staticGasOptions = { gasLimit: defaultGasLimit };
     }
 
-    chain.staticGasOptions.gasPrice = gasPrice;
+    const minGasPrice = ((minGasPrices[env] || {})[chain.name.toLowerCase()] || 0) * 1e9;
+    chain.staticGasOptions.gasPrice = gasPrice < minGasPrice ? minGasPrice : gasPrice;
+
+    printInfo(`${chain.name} static gas price set to`, `${chain.staticGasOptions.gasPrice / 1e9} gwei`);
+
     printInfo(`staticGasOptions updated succesfully and stored in config file`);
 }
 
@@ -37,7 +67,7 @@ async function main(options) {
 
 const program = new Command();
 
-program.name('update-static-gas-options').description('Update staticGasOptions to be used when offline signing');
+program.name('update-static-gas-options').description('Update staticGasOptions');
 
 program.addOption(
     new Option('-e, --env <env>', 'environment')
