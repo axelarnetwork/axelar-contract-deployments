7b7fa87376be0e136a25a313a77004c636f9a7ee
tilacog <tilacog@gmail.com>
2023-11-16 13:57:53 -0300

    solana:gateway: remove unecessary unwraps

--- solana/programs/gateway/src/lib.rs
+++ solana/programs/gateway/src/lib.rs
@@ -23,38 +23,34 @@ pub mod gateway {
             destination_contract_address,
             payload,
         )
-        .unwrap();
-        Ok(())
     }
 
     pub fn is_contract_call_approved(
         ctx: Context<IsContractCallApproved>,
         seeds_hash: [u8; 32],
     ) -> Result<bool> {
-        Ok(instructions::is_contract_call_approved(ctx, seeds_hash).unwrap())
+        instructions::is_contract_call_approved(ctx, seeds_hash)
     }
 
     pub fn validate_contract_call(
         ctx: Context<ValidateContractCall>,
         seeds_hash: [u8; 32],
     ) -> Result<bool> {
-        Ok(instructions::validate_contract_call(ctx, seeds_hash).unwrap())
+        instructions::validate_contract_call(ctx, seeds_hash)
     }
 
     pub fn execute(ctx: Context<Execute>, seeds_hash: [u8; 32]) -> Result<()> {
-        instructions::execute(ctx, seeds_hash).unwrap();
-        Ok(())
+        instructions::execute(ctx, seeds_hash)
     }
 
     pub fn auth_module(ctx: Context<AuthModule>) -> Result<()> {
-        instructions::auth_module(ctx).unwrap();
-        Ok(())
+        instructions::auth_module(ctx)
     }
 
     pub fn is_command_executed(
         ctx: Context<IsCommandExecuted>,
         command_id: [u8; 32],
     ) -> Result<bool> {
-        Ok(instructions::is_command_executed(ctx, command_id).unwrap())
+        instructions::is_command_executed(ctx, command_id)
     }
 }
