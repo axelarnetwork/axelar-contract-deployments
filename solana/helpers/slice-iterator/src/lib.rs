4700dc175a487f9505b016dab67d0982ce484976
tilacog <tilacog@gmail.com>
2023-11-29 17:31:39 -0300

    solana:helpers:slice-iterator: don't encode collection count

--- solana/helpers/slice-iterator/src/lib.rs
+++ solana/helpers/slice-iterator/src/lib.rs
@@ -7,31 +7,13 @@ use std::mem::size_of;
 pub struct SliceIterator<'a> {
     input: &'a [u8],
     cursor: u16,
-    size: u16,
 }
 const STEP: usize = size_of::<u16>();
 
 impl<'a> SliceIterator<'a> {
     /// Creates a new `SliceIterator` for the given byte slice.
-    pub fn new(input: &'a [u8]) -> Result<Self, IterationError> {
-        // The first two bytes should contain the collection size
-        if input.len() < STEP {
-            return Err(IterationError {
-                kind: InputError::TooSmall,
-                position: 0,
-            });
-        }
-
-        let (collection_size_bytes, rest) = input.split_at(STEP);
-        // Unwrap: We just checked that `collection_size_bytes` contains at least two bytes.
-        let collection_size_bytes = collection_size_bytes.try_into().unwrap();
-        let collection_size = u16::from_be_bytes(collection_size_bytes);
-
-        Ok(Self {
-            input: rest,
-            cursor: 0,
-            size: collection_size,
-        })
+    pub fn new(input: &'a [u8]) -> Self {
+        Self { input, cursor: 0 }
     }
 
     /// Generates an `IterationError` with specific `InputError` and current cursor position.
@@ -40,11 +22,6 @@ impl<'a> SliceIterator<'a> {
             kind,
             position: self.cursor,
         }
-    }
-
-    // The initial collection size.
-    pub fn size(&self) -> u16 {
-        self.size
     }
 }
 
@@ -102,17 +79,14 @@ impl<'a> Iterator for SliceIterator<'a> {
 /// This function expects the input byte slice to be formatted as a sequence of sub-slices,
 /// where each sub-slice is prefixed with its length encoded as a big-endian `u16`.
 pub fn deserialize_slices(src: &[u8]) -> Result<Vec<&[u8]>, IterationError> {
-    SliceIterator::new(src)?.collect()
+    SliceIterator::new(src).collect()
 }
 
 /// Serializes slices into a writer.
 ///
 /// Each slice is prefixed with its length encoded as a big-endian `u16`.
 pub fn serialize_slices<W: Write>(src: &[&[u8]], writer: &mut W) -> io::Result<()> {
-    // write the collection size in the first two bytes
-    writer.write_all(&(src.len() as u16).to_be_bytes())?;
-    // Then write each sub-slice prefixed by the sub-slice size
-    for value in src {
+    for &value in src {
         writer.write_all(&(value.len() as u16).to_be_bytes())?;
         writer.write_all(value)?;
     }
@@ -122,25 +96,6 @@ pub fn serialize_slices<W: Write>(src: &[&[u8]], writer: &mut W) -> io::Result<(
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn iterator_initialization() {
-        let a = &[1, 2, 3, 4, 5];
-        let b = &[5, 6, 7, 8, 9, 10];
-        let data: &[&[u8]] = &[a, b];
-        let mut buffer = vec![];
-        serialize_slices(data, &mut buffer).unwrap();
-        assert_eq!(u16::from_be_bytes([buffer[0], buffer[1]]), 2); // collection size
-
-        // Iterator should consume the first two bytes of the slice when it is created.
-        let mut iterator = SliceIterator::new(&buffer).unwrap();
-        assert_eq!(iterator.input, &buffer[2..]);
-        assert_eq!(iterator.size, 2);
-        let first = iterator.next().unwrap().unwrap();
-        assert_eq!(first, a);
-        let second = iterator.next().unwrap().unwrap();
-        assert_eq!(second, b);
-    }
 
     #[test]
     fn iterator_round_trip() {
@@ -162,11 +117,10 @@ mod tests {
         let data: &[&[u8]] = &[&[12, 13]];
         let mut buffer = vec![];
         serialize_slices(data, &mut buffer).unwrap();
-        assert_eq!(u16::from_be_bytes([buffer[0], buffer[1]]), 1); // collection size
-        assert_eq!(u16::from_be_bytes([buffer[2], buffer[3]]), 2); // first sub-slice size
-        assert_eq!(buffer[4], 12); // first sub-slice element
-        assert_eq!(buffer[5], 13); // second sub-slice element
-        assert!(buffer.get(6).is_none());
+        assert_eq!(u16::from_be_bytes([buffer[0], buffer[1]]), 2); // sub-slice size
+        assert_eq!(buffer[2], 12); // sub-slice element
+        assert_eq!(buffer[3], 13); // sub-slice element
+        assert!(buffer.get(4).is_none());
     }
 
     #[test]
@@ -176,15 +130,14 @@ mod tests {
         let data: &[&[u8]] = &[a, b];
         let mut buffer = vec![];
         serialize_slices(data, &mut buffer).unwrap();
-        assert_eq!(u16::from_be_bytes([buffer[0], buffer[1]]), 2); // collection size
-        assert_eq!(u16::from_be_bytes([buffer[2], buffer[3]]), 2); // first sub-slice size
-        assert_eq!(buffer[4], 20); // first sub-slice, first element
-        assert_eq!(buffer[5], 21); // second sub-slice, second element
-        assert_eq!(u16::from_be_bytes([buffer[6], buffer[7]]), 3); // second sub-slice size
-        assert_eq!(buffer[8], 30); // second sub-slice, first element
-        assert_eq!(buffer[9], 31); // second sub-slice, second element
-        assert_eq!(buffer[10], 32); // secont sub-slice, third element
-        assert!(buffer.get(11).is_none());
+        assert_eq!(u16::from_be_bytes([buffer[0], buffer[1]]), 2); // first sub-slice size
+        assert_eq!(buffer[2], 20); // first sub-slice, first element
+        assert_eq!(buffer[3], 21); // second sub-slice, second element
+        assert_eq!(u16::from_be_bytes([buffer[4], buffer[5]]), 3); // second sub-slice size
+        assert_eq!(buffer[6], 30); // second sub-slice, first element
+        assert_eq!(buffer[7], 31); // second sub-slice, second element
+        assert_eq!(buffer[8], 32); // secont sub-slice, third element
+        assert!(buffer.get(9).is_none());
         // round-trip
         assert_eq!(deserialize_slices(&buffer).unwrap(), data);
     }
@@ -194,9 +147,8 @@ mod tests {
         let data: &[&[u8]] = &[&[]];
         let mut buffer = vec![];
         serialize_slices(data, &mut buffer).unwrap();
-        assert_eq!(u16::from_be_bytes([buffer[0], buffer[1]]), 1); // collection-size
-        assert_eq!(u16::from_be_bytes([buffer[2], buffer[3]]), 0); // first sub-slice size
-        assert!(buffer.get(4).is_none());
+        assert_eq!(u16::from_be_bytes([buffer[0], buffer[1]]), 0); // first sub-slice size
+        assert!(buffer.get(3).is_none());
         // round-trip
         assert_eq!(deserialize_slices(&buffer).unwrap(), data);
     }
@@ -206,8 +158,7 @@ mod tests {
         let data: &[&[u8]] = &[];
         let mut buffer = vec![];
         serialize_slices(data, &mut buffer).unwrap();
-        assert_eq!(u16::from_be_bytes([buffer[0], buffer[1]]), 0);
-        assert!(buffer.get(2).is_none());
+        assert!(buffer.is_empty());
         // round-trip
         assert_eq!(deserialize_slices(&buffer).unwrap(), data);
     }
