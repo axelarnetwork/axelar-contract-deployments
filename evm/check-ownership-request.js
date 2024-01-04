0af8cb7ae356664d22312688d115e495911b6334
blockchainguyy <ayusht11@outlook.com>
2024-01-04 20:34:29 +0530

    add: script to check contract address before approving ownership request

---
+++ evm/check-ownership-request.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const { Command, Option } = require('commander');
+const { ethers } = require('hardhat');
+const { getDefaultProvider, Contract } = ethers;
+
+const { addBaseOptions } = require('./cli-utils');
+const { printError, mainProcessor, printInfo } = require('./utils');
+
+const ERC20 = require('@axelar-network/axelar-cgp-solidity/artifacts/contracts/ERC20.sol/ERC20.json');
+
+async function processCommand(_, chain, options) {
+    const { rpc } = options;
+    let { address, addresses } = options;
+
+    try {
+        addresses = JSON.parse(addresses);
+    } catch (error) {
+        printError(`Error while parsing addresses: `, error.message);
+    }
+
+    const provider = getDefaultProvider(rpc || chain.rpc);
+    const erc20 = new Contract(address, ERC20.abi, provider);
+
+    try {
+        await checkContract(address, addresses, erc20);
+    } catch (error) {
+        printError(`Error while checking contract address`, error.message);
+    }
+
+    printInfo(`Contract verification checks passed.`);
+}
+
+async function checkContract(address, addressesArray, erc20) {
+    const name = await erc20.name();
+    const symbol = await erc20.symbol();
+    const regex = /axl/i;
+
+    if (addressesArray.some((addr) => addr.toLowerCase() === address.toLowerCase())) {
+        throw new Error(`Contract address ${address} matches one of the specified addresses`);
+    }
+
+    if (regex.test(name) || regex.test(symbol)) {
+        throw new Error('Contract name or symbol includes "axl"');
+    }
+}
+
+async function main(options) {
+    await mainProcessor(options, processCommand);
+}
+
+if (require.main === module) {
+    const program = new Command();
+
+    program
+        .name('check-ownership-request')
+        .description('Before signing the message check that the contract address provided is not for AXL or any of our important contract');
+
+    addBaseOptions(program, { ignorePrivateKey: true, address: true });
+
+    program.addOption(new Option('-r, --rpc <rpc>', 'The rpc url for creating a provider to fetch gasOptions'));
+    program.addOption(
+        new Option('--addresses <addresses>', 'The Array of addresses for which the address to check against').env('ADDRESSES'),
+    );
+
+    program.action((options) => {
+        main(options);
+    });
+
+    program.parse();
+}
