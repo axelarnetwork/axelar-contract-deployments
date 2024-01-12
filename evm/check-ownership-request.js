7395024be5cb98fc205913b6fa923a192ca776e3
blockchainguyy <ayusht11@outlook.com>
2024-01-12 16:04:05 +0530

    refactor: resolve PR comments

--- evm/check-ownership-request.js
+++ evm/check-ownership-request.js
@@ -10,14 +10,15 @@ const {
 } = ethers;
 
 const { addBaseOptions } = require('./cli-utils');
-const { printError, mainProcessor, printInfo } = require('./utils');
-
-const ERC20 = require('@axelar-network/axelar-cgp-solidity/artifacts/contracts/ERC20.sol/ERC20.json');
-const IInterchainToken = require('@axelar-network/interchain-token-service/interfaces/IInterchainToken.json');
+const { printError, mainProcessor, printInfo, getContractJSON, isValidAddress } = require('./utils');
 
 async function processCommand(_, chain, options) {
     const { rpc } = options;
     let { address, addresses } = options;
+
+    if(!isValidAddress(address)) {
+        throw new Error('Invalid address parameter.');
+    }
 
     try {
         addresses = JSON.parse(addresses);
@@ -25,8 +26,15 @@ async function processCommand(_, chain, options) {
         printError(`Error while parsing addresses: `, error.message);
     }
 
+    for(const index in addresses) {
+        if(!isValidAddress(addresses[index])) {
+            throw new Error(`Invalid address at index ${index} in addresses parameter.`);
+        }
+    }
+
     const provider = getDefaultProvider(rpc || chain.rpc);
-    const erc20 = new Contract(address, ERC20.abi, provider);
+    const erc20ABI = getContractJSON('ERC20').abi;
+    const erc20 = new Contract(address, erc20ABI, provider);
 
     try {
         await checkContract(address, addresses, erc20);
@@ -38,16 +46,8 @@ async function processCommand(_, chain, options) {
 }
 
 async function checkContract(address, addressesArray, erc20) {
-    const name = await erc20.name();
-    const symbol = await erc20.symbol();
-    const regex = /axl/i;
-
     if (addressesArray.some((addr) => addr.toLowerCase() === address.toLowerCase())) {
         throw new Error(`Contract address ${address} matches one of the specified addresses`);
-    }
-
-    if (regex.test(name) || regex.test(symbol)) {
-        throw new Error('Contract name or symbol includes "axl"');
     }
 }
 
@@ -58,11 +58,12 @@ async function checkAxelarDeployedToken(address, chain, provider) {
     try {
         const response = await axios.get(apiUrl);
         const data = response.data;
-        isAxelarDeployed = data.confirmed === true && data.is_external === true;
+        isAxelarDeployed = data.confirmed === true && data.is_external === false;
     } catch (error) {}
 
     if (!isAxelarDeployed) {
-        const interchainToken = new Contract(address, IInterchainToken.abi, provider);
+        const interchainTokenABI = getContractJSON('InterchainToken').abi;
+        const interchainToken = new Contract(address, interchainTokenABI, provider);
 
         try {
             const itsAddress = await interchainToken.interchainTokenService();
@@ -86,13 +87,13 @@ if (require.main === module) {
 
     program
         .name('check-ownership-request')
-        .description('Before signing the message check that the contract address provided is not for AXL or any of our important contract');
+        .description('Before signing the message check that the contract address provided is not for AXL or any of our other important contracts');
 
     addBaseOptions(program, { ignorePrivateKey: true, address: true });
 
     program.addOption(new Option('-r, --rpc <rpc>', 'The rpc url for creating a provider to fetch gasOptions'));
     program.addOption(
-        new Option('--addresses <addresses>', 'The Array of addresses for which the address to check against').env('ADDRESSES'),
+        new Option('--addresses <addresses>', 'The Array of addresses that the address will be checked against').env('ADDRESSES'),
     );
 
     program.action((options) => {
