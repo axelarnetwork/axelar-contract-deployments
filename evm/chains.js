f20b02778ec7949f03aa94e2528148e1ee73920e
Dean Amiel <dean@axelar.network>
2024-01-30 01:37:39 -0500

    fix: lint error

--- evm/chains.js
+++ evm/chains.js
@@ -4,7 +4,7 @@ const { Command } = require('commander');
 const { mainProcessor, printInfo } = require('./utils');
 const { addBaseOptions } = require('./cli-utils');
 
-async function processCommand(_, chain, _) {
+async function processCommand(_, chain, options) {
     printInfo('Axelar Chain Name', chain.axelarId);
 }
 
