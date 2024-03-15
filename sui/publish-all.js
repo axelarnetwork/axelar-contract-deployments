fb1e60d792d17274b7411deefbc5f313300a529a
Foivos <foivos@umich.edu>
2024-03-15 16:46:04 +0100

    prettier

--- sui/publish-all.js
+++ sui/publish-all.js
@@ -1,9 +1,6 @@
 const { SuiClient, getFullnodeUrl } = require('@mysten/sui.js/client');
 const { Ed25519Keypair } = require('@mysten/sui.js/keypairs/ed25519');
-const {
-    saveConfig,
-    loadConfig,
-} = require('../evm/utils');
+const { saveConfig, loadConfig } = require('../evm/utils');
 const { publishAll } = require('@axelar-network/axelar-cgp-sui/scripts/publish-all');
 const { getConfig, parseEnv } = require('@axelar-network/axelar-cgp-sui/scripts/utils');
 const { setTrustedAddresses } = require('@axelar-network/axelar-cgp-sui/scripts/its/set-trusted-address');
@@ -11,22 +8,18 @@ const { setItsDiscovery } = require('@axelar-network/axelar-cgp-sui/scripts/its/
 const { Command, Option } = require('commander');
 
 async function main(options) {
-    const privKey =
-        Buffer.from(
-            options.privateKey,
-            "hex"
-        );
+    const privKey = Buffer.from(options.privateKey, 'hex');
     const keypair = Ed25519Keypair.fromSecretKey(privKey);
     const client = new SuiClient({ url: getFullnodeUrl(options.env) });
     await publishAll(client, keypair, parseEnv(options.env));
 
     const config = loadConfig(options.env);
 
-    if(!config.sui) {
+    if (!config.sui) {
         config.sui = {};
     }
 
-    for(const package of ['axelar', 'governance', 'gas_service', 'its', 'abi']) {
+    for (const package of ['axelar', 'governance', 'gas_service', 'its', 'abi']) {
         config.sui[package] = getConfig(package, options.env);
     }
 
@@ -53,15 +46,21 @@ if (require.main === module) {
     //program.addOption(new Option('--gasOptions <gasOptions>', 'gas options cli override'));
 
     program.addOption(new Option('-p, --privateKey <privateKey>', 'private key').makeOptionMandatory(true).env('SUI_PRIVATE_KEY'));
-    
+
     program.addOption(
-        new Option('--validators <validatorAddresses>', 'addresses of the intiial validator set').makeOptionMandatory(true).env('SUI_INITIAL_VALIDATOR_ADDRESSES'),
+        new Option('--validators <validatorAddresses>', 'addresses of the intiial validator set')
+            .makeOptionMandatory(true)
+            .env('SUI_INITIAL_VALIDATOR_ADDRESSES'),
     );
     program.addOption(
-        new Option('--weights <validatorWeights>', 'wieghts of the intiial validator set').makeOptionMandatory(true).env('SUI_INITIAL_VALIDATOR_WEIGHTS'),
+        new Option('--weights <validatorWeights>', 'wieghts of the intiial validator set')
+            .makeOptionMandatory(true)
+            .env('SUI_INITIAL_VALIDATOR_WEIGHTS'),
     );
     program.addOption(
-        new Option('--threshold <threshold>', 'threshold for the intiial validator set').makeOptionMandatory(true).env('SUI_INITIAL_VALIDATOR_THRESHOLD'),
+        new Option('--threshold <threshold>', 'threshold for the intiial validator set')
+            .makeOptionMandatory(true)
+            .env('SUI_INITIAL_VALIDATOR_THRESHOLD'),
     );
 
     program.action(async (options) => {
