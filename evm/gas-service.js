cc119860414b25202d5b501cddccc501b27e4bef
re1ro <rma4ok@gmail.com>
2024-03-21 01:48:23 -0400

    Update evm/gas-service.js
    Co-authored-by: Milap Sheth <milap@interoplabs.io>

--- evm/gas-service.js
+++ evm/gas-service.js
@@ -30,7 +30,7 @@ async function getGasUpdates(env, chain, destinationChains) {
 
             if (!destinationConfig) {
                 printInfo(`Error: chain ${destinationChain} not found in config.`);
-                printInfo(`Skipping ${destinationChain}.`);
+                printError(`Skipping ${destinationChain}.`);
                 return null;
             }
 
