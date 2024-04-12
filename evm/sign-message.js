3e623a159e257e35dc81398ae84a77c6d5460194
blockchainguyy <ayusht11@outlook.com>
2024-04-12 13:53:53 +0530

    refactor: address PR comments

--- evm/sign-message.js
+++ evm/sign-message.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const { ethers } = require('hardhat');
-require('dotenv').config();
 const {
     utils: { isHexString, arrayify },
 } = ethers;
 const { Command, Option } = require('commander');
 const { getWallet } = require('./sign-utils');
 const { printInfo, validateParameters } = require('./utils');
+const { addBaseOptions } = require('./cli-utils');
 
 async function processCommand(options) {
     const { message, privateKey } = options;
@@ -39,8 +39,8 @@ if (require.main === module) {
 
     program.name('sign-message').description('sign a message from the user wallet');
 
+    addBaseOptions(program, { ignoreChainNames: true });
     program.addOption(new Option('-m, --message <message>', 'the message to be signed').makeOptionMandatory(true).env('MESSAGE'));
-    program.addOption(new Option('-p, --privateKey <privateKey>', 'private key').makeOptionMandatory(true).env('PRIVATE_KEY'));
 
     program.action((options) => {
         main(options);
