1f1f24aa2b03b520cf92087c69e4c49674d35a25
tilacog <tilacog@gmail.com>
2024-05-08 10:29:46 -0300

    chore: use statically typed database queries (#273)
    * chore: use sqlx query! macro
    
    * chore: prepared query files
    
    * docs: update docs on database usage

--- offchain/components/relayer/README.md
+++ offchain/components/relayer/README.md
@@ -2,15 +2,32 @@
 
 ## Building
 
-1. Fetch all git submodules.
+### 1. Fetch all git submodules.
 ```sh
 $ git submodule update --remote
 ```
 
-1. Compile the `relayer` package.
+### 2. Update the static/prepared `sqlx` query files.
+
+> Note: this step is required only if sqlx queries were modified.
+
+To update the `.sqlx` query files, use this command with an active connection to a fully migrated database:
+
+```sh
+$ DATABASE_URL="<postgres connection string>" cargo sqlx prepare
+```
+
+For full details, refer to the official[`sqlx` documentation on static queries](https://github.com/launchbadge/sqlx/blob/main/sqlx-cli/README.md#enable-building-in-offline-mode-with-query).
+
+### 3. Compile the `relayer` package.
+
 ```sh
 $ cargo build --release --package relayer
 ```
+
+To build the project offline, unset `DATABASE_URL` or set `SQLX_OFFLINE=true`.
+
+For full details, refer to the official [`sqlx` documentation on offline building](https://github.com/launchbadge/sqlx/blob/main/sqlx-cli/README.md#force-building-in-offline-mode).
 
 ## Running
 
@@ -18,7 +35,40 @@ $ cargo build --release --package relayer
 $ ./target/release/relayer --config <path to configuration file>
 ```
 
-### Configuration
+## Database
+
+### Migrations
+
+The Relayer depends on an active connection to a PostgreSQL database to keep track of its progress.
+
+Ensure all necessary migrations are applied to the database before starting the Relayer.
+
+To apply the required migrations, execute the following command:
+```sh
+$ sqlx migrate run --database-url "$POSTGRES_DATABASE_URL"
+```
+
+For full details, refer to the official [`sqlx` documentation on migrations](https://github.com/launchbadge/sqlx/blob/main/sqlx-cli/README.md).
+
+### Local PostgreSQL Setup with Docker
+
+#### 1. Pull the PostgreSQL Docker image
+```bash
+docker pull postgres
+```
+
+#### 2. Start a PostgreSQL container
+```bash
+docker run --name local-postgres -e POSTGRES_USER="${DB_USER}" -e POSTGRES_PASSWORD="${DB_PASSWD}" -e POSTGRES_DB="${DB_NAME}" -p 5432:5432 -d postgres
+```
+You can now use the following connection string to connect to the database:
+```
+postgresql://${DB_USER}:${DB_PASSWD}@localhost:5432/${DB_NAME}
+```
+
+This connection string can be used both in the `database.url` configuration entry an as the value for `DATABASE_URL` when preparing `.sqlx` static query files.
+
+## Configuration
 
 The Relayer is configured by a TOML file, which path is passed in the `--config` CLI argument.
 
--- offchain/components/relayer/src/state/postgres.rs
+++ offchain/components/relayer/src/state/postgres.rs
@@ -36,30 +36,30 @@ impl State {
 
     /// Updates the stored Axelar block height if the new value is greater.
     pub async fn update_axelar_block_height(&self, block_height: i64) -> Result<()> {
-        sqlx::query(
+        sqlx::query!(
             "INSERT INTO axelar_block (id, latest_block, updated_at) \
              VALUES ($1, $2, CURRENT_TIMESTAMP) \
              ON CONFLICT (id) DO UPDATE \
              SET latest_block = EXCLUDED.latest_block, updated_at = EXCLUDED.updated_at \
-             WHERE EXCLUDED.latest_block > latest_block;",
+             WHERE EXCLUDED.latest_block > axelar_block.latest_block;",
+            SINGLETON_ID,
+            block_height
         )
-        .bind(SINGLETON_ID)
-        .bind(block_height)
         .execute(&self.pool)
         .await?;
         Ok(())
     }
 
     pub async fn get_axelar_block_height(&self) -> Result<i64> {
-        let (block,): (i64,) = sqlx::query_as(
+        sqlx::query!(
             "SELECT latest_block \
              FROM axelar_block \
              WHERE id = $1;",
+            SINGLETON_ID
         )
-        .bind(SINGLETON_ID)
         .fetch_one(&self.pool)
-        .await?;
-        Ok(block)
+        .await
+        .map(|record| record.latest_block)
     }
 
     //
@@ -70,28 +70,29 @@ impl State {
     pub async fn update_solana_transaction(&self, signature: Signature) -> Result<()> {
         debug!("updating solana_transaction table");
         let signature = signature.to_string();
-        sqlx::query(
+        sqlx::query!(
             "INSERT INTO solana_transaction (id, latest_signature, updated_at) \
                      VALUES ($1, $2, CURRENT_TIMESTAMP) \
                      ON CONFLICT (id) DO UPDATE \
-                     SET signature = EXCLUDED.signature, updated_at = EXCLUDED.updated_at;",
+                     SET latest_signature = EXCLUDED.latest_signature, updated_at = EXCLUDED.updated_at;",
+            SINGLETON_ID,
+            signature
         )
-        .bind(SINGLETON_ID)
-        .bind(signature)
-        .execute(&self.pool)
-        .await?;
+        .execute(&self.pool).await?;
         Ok(())
     }
 
     #[tracing::instrument(skip(self), err, ret)]
     pub async fn get_solana_transaction(&self) -> Result<Option<Signature>> {
-        let optional_signature: Option<(String,)> = sqlx::query_as("")
-            .bind(SINGLETON_ID)
-            .fetch_optional(&self.pool)
-            .await?;
+        let optional_signature = sqlx::query!(
+            "SELECT latest_signature from solana_transaction where id = $1;",
+            SINGLETON_ID
+        )
+        .fetch_optional(&self.pool)
+        .await?;
 
         optional_signature
-            .map(|(text,)| Signature::from_str(&text))
+            .map(|record| Signature::from_str(&record.latest_signature))
             .transpose()
             .map_err(|parse_error| sqlx::Error::Decode(Box::new(parse_error)))
     }
