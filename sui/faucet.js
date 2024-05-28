f1e2c4b91019f9607ee6fd81ad24124de9b35440
Milap Sheth <milap@interoplabs.io>
2024-05-28 04:38:43 -0400

    fix faucet script

--- sui/faucet.js
+++ sui/faucet.js
@@ -2,9 +2,11 @@
 
 const { addBaseOptions } = require('cli-utils');
 const { requestSuiFromFaucetV0 } = require('@mysten/sui.js/faucet');
-const { getWallet } = require('./utils');
+const { getWallet } = require('./sign-utils');
 
 async function processCommand(config, chain, options) {
+    const keypair = await getWallet(chain, options);
+
     await requestSuiFromFaucetV0({
         host: options.faucetUrl,
         recipient: keypair.toSuiAddress(),
@@ -14,7 +16,7 @@ async function processCommand(config, chain, options) {
 if (require.main === module) {
     const program = new Command();
 
-    program.name('publish-sui-gateway-v2').description('Publish sui gateway v2');
+    program.name('faucet').description('Query the faucet for funds.');
 
     addBaseOptions(program);
 
