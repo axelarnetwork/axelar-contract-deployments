67e25cbd53434f20b3cfab419130db5e2bb50425
npty <nptytn2@gmail.com>
2024-06-04 15:41:08 +0700

    feat: complete merge coins

--- sui/manage-coins.js
+++ sui/manage-coins.js
@@ -36,12 +36,60 @@ async function getAllCoins(client, account) {
     return coinTypeToCoins;
 }
 
-async function mergeCoin(tx, coins) {
-    console.log(coins);
+async function mergeCoin(tx, coinTypeToCoins, options) {
+    const coinType = options.coinType;
+    console.log('\n==== Merging Coins ====');
+
+    if (coinType) {
+        const coins = coinTypeToCoins[coinType];
+
+        if (!coins) {
+            console.error(`No coins found for coin type ${coinType}`);
+            return;
+        }
+
+        await doMergeCoin(tx, coins.data);
+    } else {
+        for (const coinType in coinTypeToCoins) {
+            const coins = coinTypeToCoins[coinType];
+            await doMergeCoin(tx, coins.data);
+        }
+    }
+}
+
+function isGasToken(coin) {
+    return coin.coinType === '0x2::sui::SUI';
+}
+
+async function doMergeCoin(tx, coins) {
     const coinObjectIds = coins.map((coin) => coin.coinObjectId);
+
+    // If the first coin is a gas token, remove it from the list. Otherwise, the merge will fail.
+    if (isGasToken(coins[0])) {
+        coinObjectIds.shift();
+    }
+
+    if (coinObjectIds.length < 2) {
+        console.error('\nError: Need at least 2 coins to merge');
+        process.exit(0);
+    }
+
     const firstCoin = coinObjectIds.shift();
     const remainingCoins = coinObjectIds.map((id) => tx.object(id));
+
     tx.mergeCoins(firstCoin, remainingCoins);
+    console.log(`Merged ${coins.length} coins of type '${coins[0].coinType}'`);
+}
+
+function printAllCoins(coinTypeToCoins) {
+    console.log('==== Coins Info ====');
+
+    for (const coinType in coinTypeToCoins) {
+        const coins = coinTypeToCoins[coinType];
+        console.log(`Coin Type: ${coinType}`);
+        console.log(`Total Balance: ${coins.totalBalance}`);
+        console.log(`Total Objects: ${coins.data.length}`);
+    }
 }
 
 async function processCommand(config, chain, options) {
@@ -50,41 +98,30 @@ async function processCommand(config, chain, options) {
     await printWalletInfo(keypair, client, chain, options);
 
     const coinTypeToCoins = await getAllCoins(client, keypair.toSuiAddress());
+    printAllCoins(coinTypeToCoins);
+
     const tx = new TransactionBlock();
 
     if (options.merge) {
-        const coinType = options.coinType;
-
-        if (coinType) {
-            const coins = coinTypeToCoins[coinType];
-
-            if (coins) {
-                await mergeCoin(tx, coins.data);
-            } else {
-                console.error(`No coins found for coin type ${coinType}`);
-            }
-        } else {
-            for (const coinType in coinTypeToCoins) {
-                const coins = coinTypeToCoins[coinType];
-                await mergeCoin(tx, coins.data);
-            }
-        }
+        await mergeCoin(tx, coinTypeToCoins, options);
+    } else if (options.split) {
     }
 
-    if (options.split) {
+    const requireBroadcast = options.merge || options.split;
+
+    if (requireBroadcast) {
+        await client.signAndExecuteTransactionBlock({
+            transactionBlock: tx,
+            signer: keypair,
+            options: {
+                showEffects: true,
+                showObjectChanges: true,
+                showContent: true,
+            },
+        });
+
+        console.log(`\nDone`);
     }
-
-    const receipt = await client.signAndExecuteTransactionBlock({
-        transactionBlock: tx,
-        signer: keypair,
-        options: {
-            showEffects: true,
-            showObjectChanges: true,
-            showContent: true,
-        },
-    });
-
-    console.log(receipt);
 }
 
 async function mainProcessor(options, processor) {
