b287edceaef7db68e2ab48a8c929741f84583896
npty <nptytn2@gmail.com>
2024-07-13 17:01:24 +0700

    chore: add amount utils

---
+++ sui/amount-utils.js
@@ -0,0 +1,16 @@
+import { ethers } from 'ethers';
+
+// Convert formatted amount to atomic units (e.g. 1000000000). Default decimals is 9 for SUI
+function getAtomicAmount(amount, decimals = 9) {
+    return ethers.utils.parseUnits(amount, decimals).toBigInt();
+}
+
+// Convert atomic amount to formatted units (e.g. 1.0) with decimals. Default decimals is 9 for SUI
+function getFormattedAmount(amount, decimals = 9) {
+    return ethers.utils.formatUnits(amount, decimals);
+}
+
+module.exports = {
+    getAtomicAmount,
+    getFormattedAmount,
+};
