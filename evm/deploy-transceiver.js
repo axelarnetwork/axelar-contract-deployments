143fbd8b92f3c5731371fe643bfb1cc2d1849367
Foivos <foivos@umich.edu>
2024-07-17 18:09:54 +0300

    after mainnet deployments

--- evm/deploy-transceiver.js
+++ evm/deploy-transceiver.js
@@ -38,7 +38,7 @@ const library = require('../../example-wormhole-axelar-wsteth/artifacts/@wormhol
 async function processCommand(config, chain, options) {
     const gateway = chain.contracts.AxelarGateway.address;
     const gasService = chain.contracts.AxelarGasService.address
-    const nttManager = '0x66Cb5a992570EF01b522Bc59A056a64A84Bd0aAa';
+    const nttManager = '0x6981F5621691CBfE3DdD524dE71076b79F0A0278';
     
     const { privateKey, reuseProxy, reuseHelpers, reuseAuth, verify, yes, predictOnly } = options;
     const rpc = options.rpc || chain.rpc;
@@ -47,6 +47,7 @@ async function processCommand(config, chain, options) {
     const wallet = new Wallet(privateKey).connect(provider);
 
     console.log(wallet.address, await provider.getBlockNumber());
+    console.log(await provider.getFeeData());
     let deployerContract =
         options.deployMethod === 'create3' ? chain.contracts.Create3Deployer?.address : chain.contracts.ConstAddressDeployer?.address;
    
@@ -54,31 +55,31 @@ async function processCommand(config, chain, options) {
 
     //const libraryContract = await deployContract('create', wallet, library, [], options);
     //console.log(libraryContract.address);
-    const libraryAddress = 'eEcf56798CFC9e927A83F98b0112484623Cf175a';
+    const libraryAddress = '27a3daf3b243104E9b0afAe6b56026a416B852C9';
 
     const index = implementation.bytecode.indexOf('__');
 
     const toReplace = implementation.bytecode.slice(index, index+40);
     implementation.bytecode = implementation.bytecode.replace(toReplace, libraryAddress);
     
-    //const implementationContract = await deployContract('create', wallet, implementation, [gateway, gasService, nttManager], options);
+    //const implementationContract = await deployContract('create', wallet, implementation, [gateway, gasService, nttManager], {...options, gasPrice: 3000000000});
     //console.log(implementationContract.address);
-    const implementationAddress = '0x4FE90D921E279f149Ee7C7E1a79eE75803E846B1';
+    //const implementationAddress = implementationContract.address;
 
     deployerContract =
         options.proxyDeployMethod === 'create3' ? chain.contracts.Create3Deployer?.address : chain.contracts.ConstAddressDeployer?.address;
    
     options.deployerContract = deployerContract;
-    //let proxyContract = await deployContract(options.proxyDeployMethod, wallet, proxy, [implementationAddress, '0x'], options);
+    //let proxyContract = await deployContract(options.proxyDeployMethod, wallet, proxy, [implementationAddress, '0x'], {...options, gasPrice: 3000000000});
     //console.log(proxyContract.address);
     const proxyAddress = '0xaa8267908e8d2BEfeB601f88A7Cf3ec148039423';
     //await verifyContract('testnet', chain.name, proxyAddress, [implementationAddress, '0x'], {});
     
     let proxyContract = new Contract(proxyAddress, implementation.abi, wallet);
-    //await (await proxyContract.setAxelarAddress()).wait();
-    console.log(await proxyContract.populateTransaction.setAxelarChainId(10002, 'ethereum-sepolia', '0xaa8267908e8d2BEfeB601f88A7Cf3ec148039423'));
+    //await (await proxyContract.initialize()).wait();
+    console.log(await proxyContract.populateTransaction.setAxelarChainId(4, 'binance', '0xb3F5D02240d12Fc85435cf585CAB659250A24A10'));
 
-
+    await (await proxyContract.transferPauserCapability('0x6f24A47Fc8AE5441Eb47EFfC3665e70e69Ac3F05')).wait();
 }
 
 async function main(options) {
