6a16884d57fca446a0af71d42c3f6756198f9a71
eguajardo <edwin@axelar.network>
2024-07-22 18:49:48 -0600

    feat(amplifier): add instantiation governance proposal (#299)

--- cosmwasm/README.md
+++ cosmwasm/README.md
@@ -133,3 +133,55 @@ To deploy with a constant address using instantiate2, pass the `--instantiate2` 
 To upload the contract and compute the expected address without instantiating, pass `--instantiate2` and `-u`. This will write the contract address and the code id to the config file.
 A salt can be passed with `-s`. If no salt is passed but a salt is needed for constant address deployment, the contract name will be used as a salt.
 Pass `-r` to skip the upload step, and reuse the previous code id (specified in the config).
+
+### Deploying through governance proposals
+
+On networks where only governance is allowed to upload bytecode or instantiate, the script `submit-proposal` can be used to submit a governance proposal.
+
+```
+node submit-proposal.js -m [mnemonic] -a [path to contract artifacts] -c [contract name] -e [environment] -n [chain name] --proposalType [store|instantiate] -t [proposal title] -d [proposal description] -r [run as account] --deposit [deposit]
+```
+
+### Uploading bytecode through governance
+
+Example usage:
+
+```
+node cosmwasm/submit-proposal.js --proposalType store -c ServiceRegistry -t "Proposal title" -d "Proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000
+```
+
+By default, only governance will be able to instantiate the bytecode. To allow other addresses to instantiate the bytecode, pass `--instantiateAddresses [address1],[address2],[addressN]`.
+
+For transparency and security, it's strongly recommended to include the `--source` and `--builder` options in your proposal:
+
+-   `--source`: Code Source URL is a valid absolute HTTPS URI to the contract's source code.
+-   `--builder`: Builder is a valid docker image name with tag, such as "cosmwasm/workspace-optimizer-arm64:0.16.0"
+
+These options enable voters to independently verify that the proposed bytecode matches the public source code. For example: `--source "https://github.com/axelarnetwork/axelar-amplifier/tree/service-registry-v0.4.1/contracts/service-registry" --builder "cosmwasm/workspace-optimizer-arm64:0.16.0"`
+
+After a store code proposal is accepted, the code id can be retrieved using the command `axelard q wasm list-code`
+
+### Instantiating through governance
+
+Prerequisites: Submit a proposal to upload the bytecode as described in the previous section and update `codeId` in the json config manually. TODO: create a script to automate this process.
+
+Example usage:
+
+```
+node cosmwasm/submit-proposal.js --proposalType instantiate -c ServiceRegistry -t "Proposal title" -d "Proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000
+```
+
+Note: The rules for chain name specification and the use of `--instantiate2` as described in the "Deploy the contracts" and "Constant Address Deployment" sections above also apply when instantiating through governance. Refer to those sections for details on omitting chain names for certain contracts and using `--instantiate2` for address prediction.
+
+Order of execution to satisfy dependencies:
+1.  `node cosmwasm/submit-proposal.js --proposalType instantiate -c Router -t "Router roposal title" -d "Router proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000 --instantiate2 --predictOnly`
+2.  `node cosmwasm/submit-proposal.js --proposalType instantiate -c NexusGateway -t "NexusGateway roposal title" -d "NexusGateway proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000 --instantiate2 --predictOnly`
+3.  `node cosmwasm/submit-proposal.js --proposalType instantiate -c NexusGateway -t "NexusGateway roposal title" -d "NexusGateway proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000 --instantiate2 -y`
+4.  `node cosmwasm/submit-proposal.js --proposalType instantiate -c Router -t "Router roposal title" -d "Router proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000 --instantiate2 -y`
+5.  `node cosmwasm/submit-proposal.js --proposalType instantiate -c ServiceRegistry -t "ServiceRegistry roposal title" -d "ServiceRegistry proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000 --instantiate2 -y`
+6.  `node cosmwasm/submit-proposal.js --proposalType instantiate -c Rewards -t "Rewards roposal title" -d "Rewards proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000 --instantiate2 -y`
+7.  `node cosmwasm/submit-proposal.js --proposalType instantiate -c Coordinator -t "Coordinator roposal title" -d "Coordinator proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000 --instantiate2 -y`
+8.  `node cosmwasm/submit-proposal.js --proposalType instantiate -c Multisig -t "Multisig roposal title" -d "Multisig proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000 --instantiate2 -y`
+9.  `node cosmwasm/submit-proposal.js --proposalType instantiate -c VotingVerifier -t "VotingVerifier roposal title" -d "VotingVerifier proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000 --instantiate2 -y -n "avalanche"`
+10. `node cosmwasm/submit-proposal.js --proposalType instantiate -c Gateway -t "Gateway roposal title" -d "Gateway proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000 --instantiate2 -y -n "avalanche"`
+11. `node cosmwasm/submit-proposal.js --proposalType instantiate -c MultisigProver -t "MultisigProver roposal title" -d "MultisigProver proposal description" -r $RUN_AS_ACCOUNT --deposit 100000000 --instantiate2 -y -n "avalanche"`
--- cosmwasm/submit-proposal.js
+++ cosmwasm/submit-proposal.js
@@ -2,10 +2,49 @@
 
 require('dotenv').config();
 
-const { prepareWallet, prepareClient, submitStoreCodeProposal } = require('./utils');
-const { saveConfig, loadConfig, printInfo } = require('../evm/utils');
+const {
+    prepareWallet,
+    prepareClient,
+    getChains,
+    decodeProposalAttributes,
+    encodeStoreCodeProposal,
+    encodeInstantiateProposal,
+    encodeInstantiate2Proposal,
+    submitProposal,
+    makeInstantiateMsg,
+    instantiate2AddressForProposal,
+    governanceAddress,
+} = require('./utils');
+const { saveConfig, loadConfig, printInfo, prompt } = require('../evm/utils');
+const { StoreCodeProposal, InstantiateContractProposal, InstantiateContract2Proposal } = require('cosmjs-types/cosmwasm/wasm/v1/proposal');
 
 const { Command, Option } = require('commander');
+
+const updateContractConfig = (contractConfig, chainConfig, key, value) => {
+    if (chainConfig) {
+        contractConfig[chainConfig.axelarId] = {
+            ...contractConfig[chainConfig.axelarId],
+            [key]: value,
+        };
+    } else {
+        contractConfig[key] = value;
+    }
+};
+
+const predictAndUpdateAddress = (client, contractConfig, chainConfig, options, contractName, chainName) => {
+    return instantiate2AddressForProposal(client, contractConfig, options).then((contractAddress) => {
+        updateContractConfig(contractConfig, chainConfig, 'address', contractAddress);
+
+        return contractAddress;
+    });
+};
+
+const printProposal = (proposal, proposalType) => {
+    printInfo(
+        `Encoded ${proposal.typeUrl}`,
+        JSON.stringify(decodeProposalAttributes(proposalType.toJSON(proposalType.decode(proposal.value))), null, 2),
+    );
+};
 
 const storeCode = (client, wallet, config, options) => {
     const { contractName } = options;
@@ -15,20 +54,95 @@ const storeCode = (client, wallet, config, options) => {
         },
     } = config;
 
-    submitStoreCodeProposal(client, wallet, config, options).then((proposalId) => {
+    const proposal = encodeStoreCodeProposal(options);
+
+    printProposal(proposal, StoreCodeProposal);
+
+    if (prompt(`Proceed with proposal submission?`, options.yes)) {
+        return Promise.resolve();
+    }
+
+    return submitProposal(client, wallet, config, options, proposal).then((proposalId) => {
         printInfo('Proposal submitted', proposalId);
 
         contractConfig.storeCodeProposalId = proposalId;
     });
 };
 
+const instantiate = (client, wallet, config, options, chainName) => {
+    const { contractName, instantiate2, predictOnly } = options;
+    const {
+        axelar: {
+            contracts: { [contractName]: contractConfig },
+        },
+        chains: { [chainName]: chainConfig },
+    } = config;
+
+    if (predictOnly) {
+        return predictAndUpdateAddress(client, contractConfig, chainConfig, options, contractName, chainName);
+    }
+
+    const initMsg = makeInstantiateMsg(contractName, chainName, config);
+
+    let proposal;
+
+    if (instantiate2) {
+        proposal = encodeInstantiate2Proposal(config, options, initMsg);
+        printProposal(proposal, InstantiateContract2Proposal);
+    } else {
+        proposal = encodeInstantiateProposal(config, options, initMsg);
+        printProposal(proposal, InstantiateContractProposal);
+    }
+
+    if (prompt(`Proceed with proposal submission?`, options.yes)) {
+        return Promise.resolve();
+    }
+
+    return submitProposal(client, wallet, config, options, proposal).then((proposalId) => {
+        printInfo('Proposal submitted', proposalId);
+
+        updateContractConfig(contractConfig, chainConfig, 'instantiateProposalId', proposalId);
+
+        if (instantiate2) {
+            return predictAndUpdateAddress(client, contractConfig, chainConfig, options, contractName, chainName);
+        }
+    });
+};
+
 const main = async (options) => {
-    const { env } = options;
+    const { env, proposalType, contractName } = options;
     const config = loadConfig(env);
 
     await prepareWallet(options)
         .then((wallet) => prepareClient(config, wallet))
-        .then(({ wallet, client }) => storeCode(client, wallet, config, options))
+        .then(({ wallet, client }) => {
+            switch (proposalType) {
+                case 'store':
+                    return storeCode(client, wallet, config, options);
+
+                case 'instantiate': {
+                    const chains = getChains(config, options);
+
+                    return chains.reduce((promise, chain) => {
+                        return promise.then(() =>
+                            instantiate(client, wallet, config, options, chain.toLowerCase()).then((contractAddress) => {
+                                if (contractAddress) {
+                                    printInfo(
+                                        `Predicted address for ${
+                                            chain.toLowerCase() === 'none' ? '' : chain.toLowerCase().concat(' ')
+                                        }${contractName}. Address`,
+                                        contractAddress,
+                                    );
+                                }
+                            }),
+                        );
+                    }, Promise.resolve());
+                }
+
+                default:
+                    throw new Error('Invalid proposal type');
+            }
+        })
         .then(() => saveConfig(config, env));
 };
 
@@ -47,15 +161,26 @@ const programHandler = () => {
     program.addOption(new Option('-m, --mnemonic <mnemonic>', 'mnemonic').makeOptionMandatory(true).env('MNEMONIC'));
     program.addOption(new Option('-a, --artifactPath <artifactPath>', 'artifact path').makeOptionMandatory(true).env('ARTIFACT_PATH'));
     program.addOption(new Option('-c, --contractName <contractName>', 'contract name').makeOptionMandatory(true));
+    program.addOption(new Option('-n, --chainNames <chainNames>', 'chain names').default('none'));
 
+    program.addOption(new Option('-s, --salt <salt>', 'salt for instantiate2. defaults to contract name').env('SALT'));
+    program.addOption(
+        new Option('--admin <address>', 'when instantiating contract, set an admin address. Defaults to governance module account').default(
+            governanceAddress,
+        ),
+    );
+    program.addOption(new Option('--instantiate2', 'use instantiate2 for constant address deployment'));
     program.addOption(new Option('--aarch64', 'aarch64').env('AARCH64').default(false));
     program.addOption(new Option('-y, --yes', 'skip prompt confirmation').env('YES'));
 
     program.addOption(new Option('-t, --title <title>', 'title of proposal').makeOptionMandatory(true));
     program.addOption(new Option('-d, --description <description>', 'description of proposal').makeOptionMandatory(true));
     program.addOption(new Option('--deposit <deposit>', 'the proposal deposit').makeOptionMandatory(true));
-
     program.addOption(new Option('-r, --runAs <runAs>', 'the address that will execute the message').makeOptionMandatory(true));
+    program.addOption(
+        new Option('--proposalType <proposalType>', 'proposal type').choices(['store', 'instantiate']).makeOptionMandatory(true),
+    );
+    program.addOption(new Option('--predictOnly', 'output the predicted changes only').env('PREDICT_ONLY'));
 
     program.addOption(new Option('--source <source>', "a valid HTTPS URI to the contract's source code"));
     program.addOption(
