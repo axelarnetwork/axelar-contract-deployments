f8bf76600ccc8976de09bc48f6b7b9a36ac92a09
Blockchain Guy <ayusht11@outlook.com>
2024-09-18 10:21:37 +0530

    refactor: add chaintype and connectionType param for all chains (#371)

--- evm/deploy-amplifier-gateway.js
+++ evm/deploy-amplifier-gateway.js
@@ -281,6 +281,8 @@ async function deploy(config, chain, options) {
     contractConfig.previousSignersRetention = options.previousSignersRetention;
     contractConfig.domainSeparator = domainSeparator;
     contractConfig.minimumRotationDelay = minimumRotationDelay;
+    contractConfig.connectionType = 'amplifier';
+    contractConfig.chainType = 'evm';
 
     if (options.deployMethod !== 'create') {
         contractConfig.salt = salt;
