b57a932e90fade41611633d2dadb2fd8687e8608
npty <nptytn2@gmail.com>
2024-09-19 19:05:05 +0700

    chore: remove if condition

--- sui/deploy-contract.js
+++ sui/deploy-contract.js
@@ -275,12 +275,10 @@ async function upgrade(keypair, client, supportedPackage, policy, config, chain,
 
     validateParameters({ isNonEmptyString: { packageName } });
 
-    if (options.env !== 'local') {
-        const packageDependencies = getLocalDependencies(packageDir, moveDir);
-        for (const { name } of packageDependencies) {
-            const packageAddress = contractsConfig[name]?.address;
-            updateMoveToml(packageDir, packageAddress, moveDir);
-        }
+    const packageDependencies = getLocalDependencies(packageDir, moveDir);
+    for (const { name } of packageDependencies) {
+        const packageAddress = contractsConfig[name]?.address;
+        updateMoveToml(packageDir, packageAddress, moveDir);
     }
 
     const builder = new TxBuilder(client);
