23c1eff2e785dec27a749d0c876d0073924a9652
npty <nptytn2@gmail.com>
2024-10-24 16:58:17 +0700

    chore: print env for deployment script

--- sui/deploy-contract.js
+++ sui/deploy-contract.js
@@ -333,6 +333,7 @@ async function upgrade(keypair, client, supportedPackage, policy, config, chain,
 async function mainProcessor(args, options, processor) {
     const config = loadConfig(options.env);
     const [keypair, client] = getWallet(config.sui, options);
+    printInfo('Environment', options.env);
     await printWalletInfo(keypair, client, config.sui, options);
     await processor(keypair, client, ...args, config, config.sui, options);
     saveConfig(config, options.env);
