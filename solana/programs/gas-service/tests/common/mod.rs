e1a6b40951b7a6504788b6cacbd771763bc6a25b
eloylp <eloylp@eloylp.dev>
2025-01-09 14:50:41 +0100

    chore: remove old gas service (#609)

--- solana/programs/gas-service/tests/common/mod.rs
+++
@@ -1,9 +0,0 @@
-use solana_program_test::{processor, ProgramTest};
-
-pub fn program_test() -> ProgramTest {
-    ProgramTest::new(
-        "gas_service",
-        gas_service::id(),
-        processor!(gas_service::processor::Processor::process_instruction),
-    )
-}
