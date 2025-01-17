aa5a6fbc6ff784c4227f3d2e94763952c8a80c76
Noah Bayindirli 🥂 <noah@primeprotocol.xyz>
2025-01-17 10:11:13 -0700

    feat(stellar::deploy-contract): support ITS example

--- stellar/deploy-contract.js
+++ stellar/deploy-contract.js
@@ -89,8 +89,9 @@ async function getInitializeArgs(config, chain, contractName, wallet, options) {
         case 'example': {
             const gatewayAddress = nativeToScVal(Address.fromString(chain?.contracts?.axelar_gateway?.address), { type: 'address' });
             const gasServiceAddress = nativeToScVal(Address.fromString(chain?.contracts?.axelar_gas_service?.address), { type: 'address' });
+            const itsHubAddress = nativeToScVal(config.axelar?.contracts?.InterchainTokenService?.address, { type: 'string' });
 
-            return { gatewayAddress, gasServiceAddress };
+            return { gatewayAddress, gasServiceAddress, itsHubAddress };
         }
 
         default:
