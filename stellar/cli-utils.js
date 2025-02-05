163d5019c4343576ec219e8209a9887efdd4970e
ahramy <ahram@interoplabs.io>
2025-02-05 12:35:57 -0800

    Update cli-utils.js

--- stellar/cli-utils.js
+++ stellar/cli-utils.js
@@ -1,26 +1,3 @@
 'use strict';
 
-const { Command } = require('commander');
-const { ethers } = require('hardhat');
-const {
-    utils: { hexlify },
-} = ethers;
-
-async function stellarAddressToBytes(address) {
-    console.log(hexlify(Buffer.from(address, 'ascii')));
-}
-
-if (require.main === module) {
-    const program = new Command();
-
-    program.name('cli-utils').description('Stellar CLI Utils.');
-
-    program
-        .command('stellar-address-to-bytes <address>')
-        .description('stellar address to bytes format')
-        .action((address) => {
-            stellarAddressToBytes(address);
-        });
-
-    program.parse();
-}
+require('dotenv').config();
