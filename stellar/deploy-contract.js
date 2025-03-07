9d663c900c51064157bb89b94c6999dbe347592d
Christian Gorenflo <christian@interoplabs.io>
2025-03-07 11:10:03 -0500

    formatting

--- stellar/deploy-contract.js
+++ stellar/deploy-contract.js
@@ -353,7 +353,9 @@ function main() {
             .addOption(new Option('--wasm-path <wasmPath>', 'path to the WASM file'))
             .addOption(new Option('--new-version <newVersion>', 'new version of the contract'))
             .addOption(new Option('--migration-data <migrationData>', 'migration data').default(null, '()'))
-            .addHelpText('after', `
+            .addHelpText(
+                'after',
+                `
 Examples:
   # using Vec<Address> as migration data:
   $ deploy-contract upgrade axelar-operators deploy --wasm-path {releasePath}/stellar_axelar_operators.optimized.wasm --new-version 2.1.7 --migration-data '["GDYBNA2LAWDKRSCIR4TKCB5LJCDRVUWKHLMSKUWMJ3YX3BD6DWTNT5FW"]'
@@ -363,7 +365,8 @@ Examples:
   
   # equivalent explicit void migration data:
   $ deploy-contract upgrade axelar-gateway deploy --wasm-path {releasePath}/stellar_axelar_gateway.optimized.wasm --new-version 1.0.1 --migration-data '()'
-            `)
+`,
+            )
             .action((options) => {
                 options.migrationData = sanitizeMigrationData(options.migrationData);
                 mainProcessor(options, upgrade, contractName);
