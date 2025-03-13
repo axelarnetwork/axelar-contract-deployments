5c5cb1d3bc01f536b68cdcccbecb13304aaf8b4b
Foivos <foivos@umich.edu>
2025-03-13 17:27:21 +0200

    added some comments

--- evm/migrate-all-tokens.js
+++ evm/migrate-all-tokens.js
@@ -11,6 +11,17 @@ const fs = require('fs');
 const toml = require('toml');
 const { getWallet } = require('./sign-utils');
 
+/*
+To use this script first configure your .env file as follows
+
+PRIVATE_KEY=${its owner private key}                                                                                         
+ENV=${local if you forked the network first, for testing, or mainnet}
+CHAINS=${all to migrate everything, or a comma separated list of chains}
+
+I suggest doing a one chain at a time at first, to make sure it doesn't take too long, and then a few at a time.
+./axelar-chains-config/tokenManagers.json has all the chains that are valid (had ITS before the update).
+*/
+
 const RPCs = toml.parse(fs.readFileSync('./axelar-chains-config/info/rpcs.toml', 'utf-8'));
 const itsAddresses = {
     ethereum: '0xB5FB4BE02232B1bBA4dC8f81dc24C26980dE9e3C',
@@ -71,9 +82,14 @@ async function migrateTokens(name) {
 }
 
 (async () => {
+    let chains;
+    if(process.env.CHAINS && process.env.CHAINS != 'all') {
+        chains = process.env.CHAINS.split(',');
+    }
     for (const name of Object.keys(info.chains)) {
-        console.log(name);
-
+        if(chains && chains.findIndex((chainName) => chainName == name) == -1) {
+            continue;
+        }
         try {
             await migrateTokens(name);
         } catch (e) {
