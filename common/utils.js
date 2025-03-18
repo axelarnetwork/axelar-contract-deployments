b5341eb4207d4bcbdb1c00d3a8b32e98965a554f
ahramy <ahram@interoplabs.io>
2025-03-18 16:23:50 -0700

    Update utils.js

--- common/utils.js
+++ common/utils.js
@@ -541,7 +541,7 @@ function asciiToBytes(string) {
  *       - Additional encoding logic can be added for new chain types.
  */
 function encodeITSDestination(config, destinationChain, destinationAddress) {
-    const { chainType } = config.chains[destinationChain] || config[destinationChain];
+    const chainType = config.chains[destinationChain]?.chainType || config[destinationChain]?.chainType;
 
     switch (chainType) {
         case 'stellar':
