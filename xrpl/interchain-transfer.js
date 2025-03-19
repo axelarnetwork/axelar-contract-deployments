063a5937e5e77b88ef65eafe06e30a365e21e3f3
k4m4 <nikolaskam@gmail.com>
2025-03-18 22:43:28 -0400

    refactor(xrpl): rename transfer to interchain-transfer

--- xrpl/interchain-transfer.js
+++ xrpl/interchain-transfer.js
@@ -2,7 +2,7 @@ const { Command, Option } = require('commander');
 const { mainProcessor, hex, parseTokenAmount } = require('./utils');
 const { addBaseOptions, addSkipPromptOption } = require('./cli-utils');
 
-async function transfer(_config, wallet, client, chain, options, args) {
+async function interchainTransfer(_config, wallet, client, chain, options, args) {
     await client.sendPayment(wallet, {
         destination: chain.contracts.InterchainTokenService.address,
         amount: parseTokenAmount(args.token, args.amount), // token is either "XRP" or "<currency>.<issuer-address>"
@@ -20,13 +20,13 @@ if (require.main === module) {
     const program = new Command();
 
     program
-        .name('transfer')
-        .description('Initiate a token transfer and/or GMP from XRPL.')
+        .name('interchain-transfer')
+        .description('Initiate an interchain token transfer from XRPL.')
         .arguments('<token> <amount> <destinationChain> <destinationAddress>')
         .addOption(new Option('--payload <payload>', 'payload to call contract at destination address with'))
         .addOption(new Option('--gasFeeAmount <gasFeeAmount>', 'gas fee amount').default('0'))
         .action((token, amount, destinationChain, destinationAddress, options) => {
-            mainProcessor(transfer, options, { token, amount, destinationChain, destinationAddress });
+            mainProcessor(interchainTransfer, options, { token, amount, destinationChain, destinationAddress });
         });
 
     addBaseOptions(program);
