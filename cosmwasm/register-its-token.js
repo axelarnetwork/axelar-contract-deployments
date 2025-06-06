5300210c5c69290d3a4bc02f9065efe71b2ba99a
Foivos <foivos@umich.edu>
2025-06-06 16:05:19 +0300

    update the script to work as a command.

--- cosmwasm/register-its-token.js
+++ cosmwasm/register-its-token.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
-const env = process.env.ENV;
-const mnemonic = process.env.MNEMONIC;
 
 const { ethers } = require('hardhat');
-const { Contract, getDefaultProvider, constants: { AddressZero } } = ethers;
-const info = require(`../axelar-chains-config/info/${env}.json`);
-const tokenInfo = require(`../axelar-chains-config/info/tokens-${env}.json`);
-const IInterchainTokenService = require('@axelar-network/interchain-token-service/artifacts/contracts/interfaces/IInterchainTokenService.sol/IInterchainTokenService.json');
-const ITokenManager = require('@axelar-network/interchain-token-service/artifacts/contracts/interfaces/ITokenManager.sol/ITokenManager.json');
+const { Contract, getDefaultProvider } = ethers;
+const { Command, Option } = require('commander');
 const IInterchainToken = require('@axelar-network/interchain-token-service/artifacts/contracts/interfaces/IInterchainToken.sol/IInterchainToken.json')
 const fs = require('fs');
-const toml = require('toml');
-const { printInfo, printError, loadConfig, saveConfig } = require('../common');
-const { initContractConfig, prepareWallet, prepareClient, instantiateContract } = require('./utils');
-const { GasPrice } = require('@cosmjs/stargate');
+const { printError, loadConfig, saveConfig } = require('../common');
+const { initContractConfig, prepareWallet, prepareClient } = require('./utils');
+const { addAmplifierOptions } = require('./cli-utils');
 
 class TokenIterator {
-    constructor() {
+    constructor(env, info, tokenInfo, options) {
+        console.log(options);
+        this.env = env
+        this.info = info;
+        this.tokenInfo = tokenInfo;
         this.tokenIndex = -1;
-        this.tokenIds = Object.keys(tokenInfo);
-        this.incrementTokenIndex();
+        this.tokenIds = options.tokenIds || Object.keys(tokenInfo);
+        this.chains = options.chains;
+        this.rpcs = options.rpcs;
+        if(!this.incrementTokenIndex()) throw new Error('No tokens found matching the provided params.');
         this.chainIndex = -1;
     }
 
@@ -28,15 +28,20 @@ class TokenIterator {
     }
 
     token() {
-        return tokenInfo[this.tokenId()];
+        return this.tokenInfo[this.tokenId()];
     }
 
     incrementTokenIndex() {
-        if(this.tokenIndex >= this.tokenIds.length - 1) return false;
-        this.tokenIndex++;
-        this.chainNames = Object.keys(this.token()).slice(0, -1);
-        this.chainIndex = 0;
-        return true;
+        while(true) {
+            if(this.tokenIndex >= this.tokenIds.length - 1) return false;
+        
+            this.tokenIndex++;
+            const token = this.token();
+            if(!token) continue;
+            this.chainNames = this.chains || Object.keys(token).slice(0, -1);
+            this.chainIndex = 0;
+            return true;
+        }
     }
 
     get() {
@@ -48,22 +53,30 @@ class TokenIterator {
     }
 
     rpc() {
+        if(this.rpcs) return this.rpcs[this.chainIndex];
+
         const chainName = this.chainName();
-        return env === 'mainnet' || env === 'testnet' ? RPCs.axelar_bridge_evm.find((chain) => chain.name.toLowerCase() === chainName).rpc_addr : info.chains[chainName].rpc;
+        return this.info.chains[chainName].rpc;
     }
 
     async getNext() {
         const previous = this.get();
         if (previous && previous.supply) delete previous.supply;
-        fs.writeFileSync(`./axelar-chains-config/info/tokens-${env}.json`, JSON.stringify(tokenInfo, null, 2));                 
+        fs.writeFileSync(`./axelar-chains-config/info/tokens-${this.env}.json`, JSON.stringify(this.tokenInfo, null, 2));                 
         while (true) {
             this.chainIndex++;
             if (this.chainIndex >= this.chainNames.length) {
                 if(!this.incrementTokenIndex()) return false;
             }
-            const chainName = this.chainName()
-            if(chainName === this.token().originChain.origin_chain) continue;
+            const chainName = this.chainName();
+
+            const token = this.token();
+            if(chainName === token.originChain.origin_chain) continue;
+
             const current = this.get();
+            
+            if (!current) continue;
+
             if (!current.registered) {
                 if (current.track) try {
                     const provider = getDefaultProvider(this.rpc());
@@ -79,13 +92,11 @@ class TokenIterator {
     }
 }
 
-async function registerToken(tokenIterator) {
-    const config = loadConfig(env);
+async function registerToken(client, wallet, tokenIterator) {
+    const config = loadConfig(tokenIterator.env);
 
     initContractConfig(config, {contractName: "InterchainTokenService"});
 
-    const wallet = await prepareWallet({mnemonic});
-    const client = await prepareClient(config, wallet);
     const supply = tokenIterator.get().supply;
     const supplyParam = supply ? {"tracked": String(supply)} : "untracked";
     const msg = { "register_p2p_token_instance": {
@@ -95,6 +106,7 @@ async function registerToken(tokenIterator) {
         "decimals": tokenIterator.get().decimals,
         "supply": supplyParam,
     } };
+    console.log(msg); return;
     const registeredChains = {};
     const registered = await client.queryContractSmart(info.axelar.contracts.InterchainTokenService.address, {"token_instance": {chain: tokenIterator.chainName(), "token_id": tokenIterator.tokenId().slice(2)}});
     if (registered) return;
@@ -117,12 +129,46 @@ async function registerToken(tokenIterator) {
     saveConfig(config, env);
 }
 
+const processCommand = async (options) => {
+    const { env } = options;
+    const info = require(`../axelar-chains-config/info/${env}.json`);
+    const tokenInfo = require(`../axelar-chains-config/info/tokens-${env}.json`);
+    const config = loadConfig(env);
+
+    if (options.rpcs && (!options.chains || options.chains.length != options.rpcs.length)) throw new Error('Need to provide chain names alongside RPCs and their length must match.');
+
+    initContractConfig(config, options);
+
+    const wallet = await prepareWallet(options);
+    const client = await prepareClient(config, wallet);
+
+    let iter = new TokenIterator(env, info, tokenInfo, options);
+
+    while (await iter.getNext()) {
+        await registerToken(client, wallet, iter);
+    }
+
+    saveConfig(config, env);
+};
+
+const programHandler = () => {
+    const program = new Command();
+
+    program.name('register-its-token').description('Register tokens to the ITS Hub.');
+
+    addAmplifierOptions(program, {});
+
+    program.addOption(new Option('-tokenIds, --tokenIds <tokenIds...>', 'tokenIds to run the script for'));
+    program.addOption(new Option('-chains, --chains <chains...>', 'chains to run the script for'));
+    program.addOption(new Option('-rpcs, --rpcs <rpcs...>', 'rpcs. Must be provided alongside a --chains argument and their length must match'));
+
+    program.action((options) => {
+        processCommand(options);
+    });
+
+    program.parse();
+};
+
 if (require.main === module) {
-    (async () => {
-        let iter = new TokenIterator();
-        await iter.getNext();await iter.getNext();
-        while(await iter.getNext()) {
-            await registerToken(iter);
-        }
-    })();
+    programHandler();
 }
