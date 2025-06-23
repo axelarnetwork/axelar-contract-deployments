0d928d74d5a570b053d65a9d9ad015c6d777a842
Andrew Jia <andrew@interoplabs.io>
2025-06-23 10:39:47 -0400

    modifying

--- common/cli-utils.js
+++ common/cli-utils.js
@@ -73,6 +73,13 @@ const addStoreOptions = (program) => {
 
     program.addOption(
         new Option(
+            '-d, --artifact-dir <artifactDir>',
+            'For Stellar: path to the contract artifact directory to upload (required if --version is not used)',
+        ).env('ARTIFACT_DIR'),
+    );
+
+    program.addOption(
+        new Option(
             '-v, --version <contractVersion>',
             'Specify a released version (X.Y.Z) or a commit hash to upload (required if --artifact-dir is not used)',
         ).env('CONTRACT_VERSION'),
