b76cb315cb90bdf3a83fe48178023acf9d72fb5a
Milap Sheth <milap@interoplabs.io>
2025-06-26 04:12:51 +0100

    re-add prettierjs

---
+++ .prettierrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    printWidth: 140,
+    singleQuote: true,
+    tabWidth: 4,
+    useTabs: false,
+    bracketSpacing: true,
+    importOrder: ['^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    plugins: ['@trivago/prettier-plugin-sort-imports'],
+    endOfLine: 'auto', // maintains existing line endings
+    proseWrap: 'preserve', // don't change line wrapping
+    overrides: [
+        {
+            files: '*.js',
+            options: {
+                trailingComma: 'all',
+            },
+        },
+        {
+            files: '*.ts',
+            options: {
+                trailingComma: 'all',
+            },
+        },
+        {
+            files: '*.json',
+            options: {
+                parser: 'json-stringify',
+            },
+        },
+        {
+            files: '*.yaml',
+        },
+    ],
+};
