a26d3b63182a952b4e1b571b7a36a3b96909a4b3
Nikola Ristić <ristic@risticnikola.com>
2025-07-03 22:29:32 +0200

    fix cli flags + parse token id as both evn and hedera format

--- hedera/cli-utils.js
+++ hedera/cli-utils.js
@@ -8,7 +8,7 @@ const addBaseOptions = (program, options = {}) => {
     program.addOption(new Option('-y, --yes', 'skip prompt confirmation').env('YES'));
 
     program.addOption(new Option('-hn, --hederaNetwork <hederaNetworkName>', 'hedera network').makeOptionMandatory(true).env('HEDERA_NETWORK'));
-    program.addOption(new Option('-p, --accountId <accountId>', 'account id').makeOptionMandatory(true).env('HEDERA_ID'));
+    program.addOption(new Option('-hid, --accountId <accountId>', 'account id').makeOptionMandatory(true).env('HEDERA_ID'));
     program.addOption(new Option('-p, --privateKey <privateKey>', 'hex encoded private key').makeOptionMandatory(true).env('PRIVATE_KEY'));
 
     return program;
