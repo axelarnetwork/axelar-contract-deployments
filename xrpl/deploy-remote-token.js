c9229996598e878fa0dda0a343b5eb1e1597c2a9
k4m4 <nikolaskam@gmail.com>
2025-07-30 18:05:46 +0300

    feat(xrpl): add deploy-remote-token script

---
+++ xrpl/deploy-remote-token.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const { Command, Option } = require('commander');
+const { addAmplifierOptions, addChainNameOption } = require('../cosmwasm/cli-utils');
+const { executeTransaction: executeCosmosTransaction } = require('../cosmwasm/utils');
+const { printInfo, printError } = require('../common');
+const { mainCosmosProcessor } = require('./utils');
+
+const deployRemoteToken = async (config, options, wallet, client, fee) => {
+    const { chainName, issuer, currency, tokenName, tokenSymbol, destinationChain } = options;
+    const [account] = await wallet.getAccounts();
+
+    const xrplGateway = config.axelar.contracts.XrplGateway[chainName];
+    if (!xrplGateway) {
+        printError(`No XRPLGateway contract found on chain ${chainName}`);
+        process.exit(1);
+    }
+
+    const deployRemoteToken = {
+        deploy_remote_token: {
+            xrpl_token: {
+                issued: {
+                    issuer,
+                    currency,
+                },
+            },
+            destination_chain: destinationChain,
+            token_metadata: {
+                name: tokenName,
+                symbol: tokenSymbol,
+            },
+        },
+    };
+
+    const { transactionHash, events } = await executeCosmosTransaction(client, account, xrplGateway.address, deployRemoteToken, fee);
+
+    printInfo('Initiated remote token deployment', transactionHash);
+
+    const contractCalledEvent = events.find((e) => e.type === 'wasm-contract_called');
+    const messageId = contractCalledEvent.attributes.find((a) => a.key === 'message_id').value;
+    const payload = contractCalledEvent.attributes.find((a) => a.key === 'payload').value;
+
+    printInfo('Message ID', messageId);
+    printInfo('Payload', payload);
+};
+
+const programHandler = () => {
+    const program = new Command();
+
+    program
+        .name('deploy-remote-token')
+        .description('Deploy XRPL IOU token on some remote chain.')
+        .addOption(new Option('--issuer <issuer>', 'XRPL address of token issuer').makeOptionMandatory(true))
+        .addOption(new Option('--currency <currency>', 'XRPL currency of token').makeOptionMandatory(true))
+        .addOption(new Option('--destinationChain <destinationChain>', 'Chain to deploy the interchain token on').makeOptionMandatory(true))
+        .addOption(new Option('--tokenName <tokenName>', 'Name of the new interchain token').makeOptionMandatory(true))
+        .addOption(new Option('--tokenSymbol <tokenSymbol>', 'Symbol of the new interchain token').makeOptionMandatory(true));
+
+    addChainNameOption(program);
+    addAmplifierOptions(program, {
+        contractOptions: false,
+    });
+
+    program.action((options) => {
+        mainCosmosProcessor(deployRemoteToken, options);
+    });
+
+    program.parse();
+};
+
+if (require.main === module) {
+    programHandler();
+}
