d1fd6c06a6f81685a05a70a43efb53819e432d67
SDavidson1177 <solomon@interoplabs.io>
2025-08-06 17:37:05 -0400

    run prettier

--- cosmwasm/contract.ts
+++ cosmwasm/contract.ts
@@ -1,9 +1,10 @@
 'use strict';
 
 import { Command } from 'commander';
+
 import { loadConfig } from '../common';
-import { prepareDummyWallet, prepareClient } from './utils';
 import { addAmplifierQueryOptions } from './cli-utils';
+import { prepareClient, prepareDummyWallet } from './utils';
 
 // cosmwasm-stargate imports protobufjs which does not have a default export
 // Consequently, import CosmWasmClient using CommonJS to avoid error TS1192
@@ -11,37 +12,40 @@ import { addAmplifierQueryOptions } from './cli-utils';
 const { CosmWasmClient } = require('@cosmjs/cosmwasm-stargate');
 
 interface ContractInfo {
-    contract: string,
-    version: string,
+    contract: string;
+    version: string;
 }
 
 function get_contract_info(client: typeof CosmWasmClient, contract_address: string): Promise<ContractInfo> {
     return new Promise(async (resolve, _) => {
-        let result = await client.queryContractRaw(contract_address, Buffer.from('contract_info'))
-        let contract_info: ContractInfo = JSON.parse(Buffer.from(result).toString('ascii'))
-        resolve(contract_info)
-    })
+        const result = await client.queryContractRaw(contract_address, Buffer.from('contract_info'));
+        const contract_info: ContractInfo = JSON.parse(Buffer.from(result).toString('ascii'));
+        resolve(contract_info);
+    });
 }
 
 const programHandler = () => {
-    let program = new Command()
+    const program = new Command();
 
-    program.name("contract").version("1.0.0").description("Query contract info")
+    program.name('contract').version('1.0.0').description('Query contract info');
 
-    addAmplifierQueryOptions(program.command("info")
-        .argument('<contract_address>', "The contract address")
-        .description("Query contract info")
-        .action(async (contract_address: string, options: { env: string }) => {
-            const { env } = options;
-            const config = loadConfig(env);
+    addAmplifierQueryOptions(
+        program
+            .command('info')
+            .argument('<contract_address>', 'The contract address')
+            .description('Query contract info')
+            .action(async (contract_address: string, options: { env: string }) => {
+                const { env } = options;
+                const config = loadConfig(env);
 
-            const wallet = await prepareDummyWallet();
-            const client = await prepareClient(config, wallet);
+                const wallet = await prepareDummyWallet();
+                const client = await prepareClient(config, wallet);
 
-            console.log(await get_contract_info(client, contract_address))
-        }))
+                console.log(await get_contract_info(client, contract_address));
+            }),
+    );
 
-    program.parse()
+    program.parse();
 };
 
 if (require.main === module) {
