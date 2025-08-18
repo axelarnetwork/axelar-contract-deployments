d868785314e284a33a05d21b5017f3b9944b62ed
ahramy <ahram@interoplabs.io>
2025-08-18 11:34:22 -0700

    Update its.js

--- evm/its.js
+++ evm/its.js
@@ -126,7 +126,7 @@ function compareToConfig(contractConfig, contractName, toCheck) {
     }
 }
 
-async function processCommand(axelar, chain, chains, action, options) {
+async function processCommand(_axelar, chain, chains, action, options) {
     const { privateKey, address, yes, args } = options;
 
     const contracts = chain.contracts;
@@ -594,7 +594,8 @@ async function processCommand(axelar, chain, chains, action, options) {
             });
 
             validateChain(chains, destinationChain);
-            const tokenManagerType = validateLinkType(getChainConfigByAxelarId(axelar, destinationChain).chainType, type);
+            const chainType = getChainConfig(chains, destinationChain, { skipCheck: true })?.chainType;
+            const tokenManagerType = validateLinkType(chainType, type);
             const interchainTokenId = await interchainTokenService.interchainTokenId(wallet.address, deploymentSalt);
             printInfo('Expected tokenId', interchainTokenId);
 
