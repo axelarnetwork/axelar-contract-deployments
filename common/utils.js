3254f0175eadd932de8f801eda0ab97cf71cbc0b
benjamin852 <weinbergben33@gmail.com>
2025-09-04 15:32:15 -0400

    chore: added appropriate testnet names for l2s

--- common/utils.js
+++ common/utils.js
@@ -796,12 +796,32 @@ function validateDestinationChain(chains, destinationChain) {
 }
 
 async function calculateItsCrossChainGas(sourceChain, destinationChain, env, eventType) {
+
     const baseUrl = env === 'mainnet' ? 'https://api.axelarscan.io/gmp' : 'https://testnet.api.gmp.axelarscan.io';
     const url = `${baseUrl}/estimateITSFee`;
 
+    const testnetL2sChainMap = {
+        optimism: 'optimism-sepolia',
+        arbitrum: 'arbitrum-sepolia',
+        base: 'base-sepolia',
+        blast: 'blast-sepolia',
+        mantle: 'mantle-sepolia',
+        linea: 'linea-sepolia',
+        scroll: 'scroll-sepolia',
+    };
+
+    const normalizeChainName = (chainName) => {
+        let chain = String(chainName).toLowerCase();
+
+        if (env === 'testnet' && testnetL2sChainMap[chain]) {
+            return testnetL2sChainMap[chain];
+        }
+        return chain;
+    };
+
     const payload = {
-        sourceChain: String(sourceChain).toLowerCase(),
-        destinationChain: String(destinationChain).toLowerCase(),
+        sourceChain: normalizeChainName(sourceChain),
+        destinationChain: normalizeChainName(destinationChain),
         event: eventType,
     };
 
@@ -811,6 +831,7 @@ async function calculateItsCrossChainGas(sourceChain, destinationChain, env, eve
     }
     return gasFee;
 }
+
 
 module.exports = {
     loadConfig,
@@ -881,5 +902,5 @@ module.exports = {
     itsHubContractAddress,
     asyncLocalLoggerStorage,
     printMsg,
-    calculateItsCrossChainGas,
+    calculateItsCrossChainGas
 };
