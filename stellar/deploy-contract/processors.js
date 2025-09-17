71c9adf8ffa8ee9c4ae0fe38d988b844b05d71cf
ahramy <ahram@interoplabs.io>
2025-09-17 11:17:17 -0700

    Update processors.js

--- stellar/deploy-contract/processors.js
+++ stellar/deploy-contract/processors.js
@@ -210,6 +210,10 @@ const getInitializeArgs = async (config, chain, contractName, wallet, options, a
                 isNumber: { decimals },
             });
 
+            if (decimals <= 0 || !Number.isInteger(decimals)) {
+                throw new Error('Decimals must be a positive integer');
+            }
+
             const tokenId = nativeToScVal(Buffer.from('0'.repeat(64), 'hex'), { type: 'bytes' });
             const tokenMetadata = tokenMetadataToScVal(decimals, name, symbol);
 
