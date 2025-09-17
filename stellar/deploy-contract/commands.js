950bf80651751a963bdb9c4e43adedfed200fbce
ahramy <ahram@interoplabs.io>
2025-09-16 20:22:53 -0700

    Update commands.js

--- stellar/deploy-contract/commands.js
+++ stellar/deploy-contract/commands.js
@@ -1,65 +1,87 @@
 'use strict';
 
-const { Command, Option } = require('commander');
-const { getContractCodePath, SUPPORTED_CONTRACTS, sanitizeMigrationData } = require('../utils');
+const { Command, Option, Argument } = require('commander');
+const { getContractCodePath, SUPPORTED_CONTRACTS, sanitizeMigrationData, addBaseOptions } = require('../utils');
 const { addStoreOptions } = require('../../common/cli-utils');
 const { mainProcessor, upgrade, upload, deploy } = require('./processors');
 
 require('../cli-utils');
 
-const CONTRACT_CONFIG = {
-    AxelarGateway: {
-        deployOptions: [
-            ['--nonce <nonce>', 'optional nonce for the signer set'],
-            ['--domain-separator <domainSeparator>', 'domain separator (keccak256 hash or "offline")', { default: 'offline' }],
-            ['--previous-signers-retention <previousSignersRetention>', 'previous signer retention', { default: 15, parser: Number }],
-            ['--minimum-rotation-delay <miniumRotationDelay>', 'minimum rotation delay', { default: 0, parser: Number }],
-        ],
-        upgradeOptions: [['--migration-data <migrationData>', 'migration data', { default: null, defaultDescription: '()' }]],
-    },
-    AxelarExample: {
-        deployOptions: [
-            ['--use-dummy-its-address', 'use dummy its address for AxelarExample contract to test a GMP call', { default: false }],
-        ],
-    },
-    AxelarOperators: {
-        upgradeOptions: [['--migration-data <migrationData>', 'migration data', { default: null, defaultDescription: '()' }]],
-    },
-    InterchainToken: {
-        args: [
-            ['<name>', 'token name (e.g., "Test Token")'],
-            ['<symbol>', 'token symbol (e.g., "TEST")'],
-            ['<decimals>', 'token decimals (e.g., 7)', parseInt],
-        ],
-        optionKeys: ['name', 'symbol', 'decimals'],
-    },
-    InterchainTokenService: {
-        deployOptions: [
-            ['--interchain-token-version <interchainTokenVersion>', 'version for InterchainToken contract', { makeRequired: true }],
-            ['--token-manager-version <tokenManagerVersion>', 'version for TokenManager contract', { makeRequired: true }],
-        ],
-        upgradeOptions: [['--migration-data <migrationData>', 'migration data', { default: null, defaultDescription: '()' }]],
-    },
+const CONTRACT_DEPLOY_OPTIONS = {
+    AxelarGateway: () => [
+        new Option('--nonce <nonce>', 'optional nonce for the signer set'),
+        new Option('--domain-separator <domainSeparator>', 'domain separator (keccak256 hash or "offline")').default('offline'),
+        new Option('--previous-signers-retention <previousSignersRetention>', 'previous signer retention').default(15).argParser(Number),
+        new Option('--minimum-rotation-delay <miniumRotationDelay>', 'minimum rotation delay').default(0).argParser(Number),
+    ],
+    AxelarExample: () => [
+        new Option('--use-dummy-its-address', 'use dummy its address for AxelarExample contract to test a GMP call').default(false),
+    ],
+    InterchainTokenService: () => [
+        new Option('--interchain-token-version <interchainTokenVersion>', 'version for InterchainToken contract').makeOptionMandatory(true),
+        new Option('--token-manager-version <tokenManagerVersion>', 'version for TokenManager contract').makeOptionMandatory(true),
+    ],
+    InterchainToken: () => [
+        new Argument('<name>', 'token name (e.g., "Test Token")'),
+        new Argument('<symbol>', 'token symbol (e.g., "TEST")'),
+        new Argument('<decimals>', 'token decimals (e.g., 7)').argParser(parseInt),
+    ],
 };
 
-const createOption = ([flag, description, config = {}]) => {
-    const option = new Option(flag, description);
-    if (config.default !== undefined) option.default(config.default, config.defaultDescription);
-    if (config.parser) option.argParser(config.parser);
-    if (config.makeRequired) option.makeOptionMandatory(true);
-    return option;
+const CONTRACT_UPGRADE_OPTIONS = {
+    AxelarGateway: () => [new Option('--migration-data <migrationData>', 'migration data').default(null, '()')],
+    AxelarOperators: () => [new Option('--migration-data <migrationData>', 'migration data').default(null, '()')],
+    InterchainTokenService: () => [new Option('--migration-data <migrationData>', 'migration data').default(null, '()')],
 };
 
-const addOptionsToCommand = (command, optionType) => {
+const CONTRACT_UPLOAD_OPTIONS = {};
+
+const addDeployOptions = (command) => {
+    addBaseOptions(command);
     addStoreOptions(command);
 
     const contractName = command.name();
-    const options = CONTRACT_CONFIG[contractName]?.[optionType];
+    const contractDeployOptions = CONTRACT_DEPLOY_OPTIONS[contractName];
 
-    if (options) {
-        options.forEach((optionConfig) => {
-            command.addOption(createOption(optionConfig));
+    if (contractDeployOptions) {
+        const items = contractDeployOptions();
+        items.forEach((item) => {
+            if (item instanceof Option) {
+                command.addOption(item);
+            } else if (item instanceof Argument) {
+                command.addArgument(item);
+            }
         });
+    }
+
+    return command;
+};
+
+const addUpgradeOptions = (command) => {
+    addBaseOptions(command);
+    addStoreOptions(command);
+
+    const contractName = command.name();
+    const contractUpgradeOptions = CONTRACT_UPGRADE_OPTIONS[contractName];
+
+    if (contractUpgradeOptions) {
+        const options = contractUpgradeOptions();
+        options.forEach((option) => command.addOption(option));
+    }
+
+    return command;
+};
+
+const addUploadOptions = (command) => {
+    addBaseOptions(command);
+    addStoreOptions(command);
+
+    const contractName = command.name();
+    const contractUploadOptions = CONTRACT_UPLOAD_OPTIONS[contractName];
+
+    if (contractUploadOptions) {
+        const options = contractUploadOptions();
+        options.forEach((option) => command.addOption(option));
     }
 
     return command;
@@ -74,41 +96,23 @@ function preActionHook(contractName) {
     };
 }
 
-const addArgumentsToCommand = (command, contractName) => {
-    const config = CONTRACT_CONFIG[contractName];
-    if (!config?.args) return;
-
-    config.args.forEach(([name, description, parser]) => {
-        command.argument(name, description, parser);
-    });
-};
-
-const createActionHandler = (contractName, processor) => {
-    const config = CONTRACT_CONFIG[contractName];
-
-    if (!config?.optionKeys) {
-        return (options) => mainProcessor(options, processor, contractName);
-    }
-
-    return (...args) => {
-        const command = args.pop();
-        const options = command.opts();
-        config.optionKeys.forEach((key, index) => {
-            options[key] = args[index];
-        });
-        mainProcessor(options, processor, contractName);
-    };
-};
-
 const getDeployContractCommands = () => {
     return Array.from(SUPPORTED_CONTRACTS).map((contractName) => {
         const command = new Command(contractName).description(`Deploy ${contractName} contract`);
 
-        addArgumentsToCommand(command, contractName);
-        addOptionsToCommand(command, 'deployOptions');
-
+        addDeployOptions(command);
         command.hook('preAction', preActionHook(contractName));
-        command.action(createActionHandler(contractName, deploy));
+
+        command.action((...args) => {
+            const cmd = args.pop();
+            const options = cmd.opts();
+            command.registeredArguments.forEach((arg, index) => {
+                if (index < args.length) {
+                    options[arg._name] = args[index];
+                }
+            });
+            mainProcessor(options, deploy, contractName);
+        });
 
         return command;
     });
@@ -118,7 +122,7 @@ const getUpgradeContractCommands = () => {
     return Array.from(SUPPORTED_CONTRACTS).map((contractName) => {
         const command = new Command(contractName).description(`Upgrade ${contractName} contract`);
 
-        addOptionsToCommand(command, 'upgradeOptions');
+        addUpgradeOptions(command);
 
         command.hook('preAction', preActionHook(contractName));
         command.action((options) => {
@@ -134,10 +138,12 @@ const getUploadContractCommands = () => {
     return Array.from(SUPPORTED_CONTRACTS).map((contractName) => {
         const command = new Command(contractName).description(`Upload ${contractName} contract`);
 
-        addOptionsToCommand(command, 'uploadOptions');
+        addUploadOptions(command);
 
         command.hook('preAction', preActionHook(contractName));
-        command.action((options) => mainProcessor(options, upload, contractName));
+        command.action((options) => {
+            mainProcessor(options, upload, contractName);
+        });
 
         return command;
     });
