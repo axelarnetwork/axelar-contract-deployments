c190add3b2a0fe0c3070dd8c49f9ee042fd9c89b
Tomasz Kulik <tomek.kulik2@gmail.com>
2025-09-24 09:33:52 +0200

    feat(cosmwasm): typescript version of mainProcessor and ConfigManager (#1024)
    Co-authored-by: Noah B. 🥂 <15343884+nbayindirli@users.noreply.github.com>

---
+++ cosmwasm/processor.ts
@@ -0,0 +1,47 @@
+import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
+import { StdFee } from '@cosmjs/stargate';
+
+import { ConfigManager, FullConfig } from '../common/config';
+import { prepareClient, prepareWallet } from './utils';
+
+type Options = {
+    env: string;
+    contractName: string;
+    chainName: string;
+    mnemonic: string;
+    runAs?: string;
+    deposit?: string;
+    instantiateAddresses?: string[];
+};
+
+type ProcessorFn = (
+    client: SigningCosmWasmClient,
+    wallet: DirectSecp256k1HdWallet,
+    config: FullConfig,
+    options: Options,
+    args?: string[],
+    fee?: string | StdFee,
+) => Promise<void>;
+
+export async function mainProcessor(processorFn: ProcessorFn, options: Options, args?: string[]) {
+    const { runAs, deposit, instantiateAddresses, env } = options;
+    const configManager = new ConfigManager(env);
+
+    options.runAs =
+        runAs ||
+        (env === 'devnet-amplifier' ? 'axelar1zlr7e5qf3sz7yf890rkh9tcnu87234k6k7ytd9' : 'axelar10d07y265gmmuvt4z0w9aw880jnsr700j7v9daj');
+    options.deposit = deposit || configManager.getProposalDepositAmount();
+    options.instantiateAddresses = instantiateAddresses || configManager.getProposalInstantiateAddresses();
+
+    configManager.initContractConfig(options.contractName, options.chainName);
+
+    const wallet = await prepareWallet(options);
+    const client = await prepareClient(configManager.getFullConfig(), wallet);
+
+    const fee = configManager.getFee();
+
+    await processorFn(client, wallet, configManager.getFullConfig(), options, args, fee);
+
+    configManager.saveConfig();
+}
