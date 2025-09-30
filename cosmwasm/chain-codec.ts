ef2db10a0b74ca1c8e5c62feb7d9a7455b23c4f8
chipshort <christoph@interoplabs.io>
2025-09-30 13:03:15 +0200

    refactor: use mainQueryProcessor for chain-codec script

--- cosmwasm/chain-codec.ts
+++ cosmwasm/chain-codec.ts
@@ -1,9 +1,12 @@
 'use strict';
 
+import { CosmWasmClient } from '@cosmjs/cosmwasm-stargate';
 import { Command } from 'commander';
 
-import { loadConfig, printInfo, printWarn, saveConfig } from '../common';
+import { printInfo, printWarn } from '../common';
 import { addEnvOption } from '../common/cli-utils';
+import { FullConfig } from '../common/config';
+import { Options, mainQueryProcessor } from './processor';
 
 const programHandler = () => {
     const program = new Command();
@@ -14,68 +17,70 @@ const programHandler = () => {
         program
             .command('prepare')
             .description('Prepare the config for chain-codec instantiation')
-            .action(async (options: { env: string }) => {
-                const { env } = options;
-                const config = loadConfig(env);
-
-                try {
-                    const codecMapping: Record<string, 'ChainCodecEvm' | 'ChainCodecSui' | 'ChainCodecStellar'> = {
-                        evm: 'ChainCodecEvm',
-                        sui: 'ChainCodecSui',
-                        stellar: 'ChainCodecStellar',
-                    };
-
-                    const chains = config?.chains || {};
-
-                    const chainTypes = Object.values(chains).map((chainConfig) => {
-                        return (chainConfig as { chainType: string })?.chainType;
-                    });
-
-                    for (const [chainName, chainConfig] of Object.entries(chains)) {
-                        const chainType = (chainConfig as { chainType: string })?.chainType;
-                        if (!chainType) {
-                            // Unsupported or unspecified chain type
-                            printWarn(`Missing chain type for chain ${chainName}; skipping ChainCodec entry`);
-                            continue;
-                        }
-
-                        const codecContractName = codecMapping[chainType];
-                        if (!codecContractName) {
-                            // Unsupported or unspecified chain type
-                            printInfo(`Unsupported chain type: ${chainType}; skipping ChainCodec entry`);
-                            continue;
-                        }
-
-                        // add ChainCodec for each chain type
-                        config.axelar.contracts[codecContractName] = config.axelar.contracts[codecContractName] || {};
-
-                        // remove addressFormat and encoder from MultisigProver and VotingVerifier config
-                        const votingVerifier = config.axelar.contracts.VotingVerifier[chainName];
-                        const multisigProver = config.axelar.contracts.MultisigProver[chainName];
-
-                        if (votingVerifier) {
-                            delete votingVerifier.addressFormat;
-                        } else {
-                            printInfo(`Missing VotingVerifier config for chain ${chainName}`);
-                        }
-
-                        if (multisigProver) {
-                            delete multisigProver.encoder;
-                        } else {
-                            printInfo(`Missing MultisigProver config for chain ${chainName}`);
-                        }
-                    }
-
-                    saveConfig(config, env);
-                    printInfo(`Chain codec preparation complete`);
-                } catch (error) {
-                    console.error(error);
-                }
+            .action(async (options) => {
+                mainQueryProcessor(prepare, options);
             }),
     );
 
     program.parse();
 };
+
+async function prepare(client: CosmWasmClient, config: FullConfig, options: Options) {
+    const { env } = options;
+
+    try {
+        const codecMapping: Record<string, 'ChainCodecEvm' | 'ChainCodecSui' | 'ChainCodecStellar'> = {
+            evm: 'ChainCodecEvm',
+            sui: 'ChainCodecSui',
+            stellar: 'ChainCodecStellar',
+        };
+
+        const chains = config.chains;
+
+        const chainTypes = Object.values(chains).map((chainConfig) => {
+            return (chainConfig as { chainType: string })?.chainType;
+        });
+
+        for (const [chainName, chainConfig] of Object.entries(chains)) {
+            const chainType = (chainConfig as { chainType: string })?.chainType;
+            if (!chainType) {
+                // Unsupported or unspecified chain type
+                printWarn(`Missing chain type for chain ${chainName}; skipping ChainCodec entry`);
+                continue;
+            }
+
+            const codecContractName = codecMapping[chainType];
+            if (!codecContractName) {
+                // Unsupported or unspecified chain type
+                printInfo(`Unsupported chain type: ${chainType}; skipping ChainCodec entry`);
+                continue;
+            }
+
+            // add ChainCodec for each chain type
+            config.axelar.contracts[codecContractName] = config.axelar.contracts[codecContractName] || {};
+
+            // remove addressFormat and encoder from MultisigProver and VotingVerifier config
+            const votingVerifier: { addressFormat?: string } = config.axelar.contracts.VotingVerifier[chainName];
+            const multisigProver: { encoder?: string } = config.axelar.contracts.MultisigProver[chainName];
+
+            if (votingVerifier) {
+                delete votingVerifier.addressFormat;
+            } else {
+                printInfo(`Missing VotingVerifier config for chain ${chainName}`);
+            }
+
+            if (multisigProver) {
+                delete multisigProver.encoder;
+            } else {
+                printInfo(`Missing MultisigProver config for chain ${chainName}`);
+            }
+        }
+
+        printInfo(`Chain codec preparation complete`);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 if (require.main === module) {
     programHandler();
