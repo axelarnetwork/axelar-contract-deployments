eigerco/axelar-amplifier-solana

refactor(its): migrate to use cpi events
Signed-off-by: Guilherme Felipe da Silva <gfsilva.eng@gmail.com>

programs/axelar-solana-its/tests/module/deploy_remote.rs
@@ -1,4 +1,4 @@
-use event_utils::Event as _;
+use anyhow::anyhow;
 use mpl_token_metadata::accounts::Metadata;
 use mpl_token_metadata::instructions::CreateV1Builder;
 use mpl_token_metadata::types::TokenStandard;
@@ -6,7 +6,7 @@ use solana_program_test::tokio;
 use solana_sdk::instruction::Instruction;
 use test_context::test_context;
 
-use crate::ItsTestContext;
+use crate::{find_event_in_inner_instructions, ItsTestContext};
 
 #[test_context(ItsTestContext)]
 #[tokio::test]
@@ -26,18 +26,24 @@ async fn test_deploy_remote_interchain_token_with_valid_metadata(
         Some(ctx.solana_wallet),
     )?;
 
-    let tx = ctx
-        .send_solana_tx(&[deploy_local_ix])
+    let simulation_result = ctx.simulate_solana_tx(&[deploy_local_ix.clone()]).await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()
+        .first()
+        .cloned()
+        .unwrap();
+    let deploy_event = find_event_in_inner_instructions::<
+        axelar_solana_its::events::InterchainTokenDeployed,
+    >(&inner_ixs)
+    .ok_or_else(|| anyhow!("InterchainTokenDeployed not found"))
+    .unwrap();
+
+    ctx.send_solana_tx(&[deploy_local_ix])
         .await
         .expect("InterchainToken deployment failed");
-
-    let deploy_event = tx
-        .metadata
-        .unwrap()
-        .log_messages
-        .iter()
-        .find_map(|log| axelar_solana_its::events::InterchainTokenDeployed::try_from_log(log).ok())
-        .unwrap();
 
     assert_eq!(
         deploy_event.name, "Valid Metadata Token",
@@ -71,6 +77,21 @@ async fn test_deploy_remote_interchain_token_with_valid_metadata(
             0,
         )?;
 
+    let simulation_result = ctx.simulate_solana_tx(&[deploy_remote_ix.clone()]).await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()
+        .first()
+        .cloned()
+        .unwrap();
+    let deployment_started_event = find_event_in_inner_instructions::<
+        axelar_solana_its::events::InterchainTokenDeploymentStarted,
+    >(&inner_ixs)
+    .ok_or_else(|| anyhow!("InterchainTokenDeploymentStarted not found"))
+    .unwrap();
+
     let tx = ctx.send_solana_tx(&[deploy_remote_ix]).await;
 
     // Transaction should succeed
@@ -78,17 +99,6 @@ async fn test_deploy_remote_interchain_token_with_valid_metadata(
         tx.is_ok(),
         "Expected deployment to succeed with valid metadata"
     );
-
-    let tx = tx.unwrap();
-    let deployment_started_event = tx
-        .metadata
-        .unwrap()
-        .log_messages
-        .iter()
-        .find_map(|log| {
-            axelar_solana_its::events::InterchainTokenDeploymentStarted::try_from_log(log).ok()
-        })
-        .unwrap();
 
     assert_eq!(
         deployment_started_event.token_name, "Valid Metadata Token",

programs/axelar-solana-its/tests/module/memo_cpi_transfer.rs
@@ -1,17 +1,16 @@
-use axelar_solana_gateway_test_fixtures::{
-    assert_msg_present_in_logs,
-    gateway::{get_gateway_events, ProgramInvocationState},
-};
+use anyhow::anyhow;
+use axelar_solana_gateway::events::CallContractEvent;
+use axelar_solana_gateway_test_fixtures::assert_msg_present_in_logs;
 use axelar_solana_its::state::token_manager::Type;
 use axelar_solana_memo_program::get_counter_pda;
 use evm_contracts_test_suite::ethers::signers::Signer as EvmSigner;
 use interchain_token_transfer_gmp::GMPPayload;
 use solana_program::pubkey::Pubkey;
-use solana_program_test::{tokio, BanksTransactionResultWithMetadata};
+use solana_program_test::tokio;
 use spl_associated_token_account::get_associated_token_address_with_program_id;
 use test_context::test_context;
 
-use crate::{BorshPdaAccount, ItsTestContext};
+use crate::{find_event_in_inner_instructions, BorshPdaAccount, ItsTestContext};
 
 /// Common test setup data
 struct TestSetup {
@@ -107,31 +106,15 @@ async fn verify_token_manager_type(ctx: &mut ItsTestContext, token_manager_pda: 
     };
 }
 
-/// Extract and verify the CallContract event from a transaction
+/// Extract and verify the CallContract event from inner instructions
 fn verify_gateway_event_and_source(
-    tx: &BanksTransactionResultWithMetadata,
+    inner_ixs: &[solana_sdk::inner_instruction::InnerInstruction],
     expected_source: &[u8; 32],
     expected_amount: u64,
 ) -> GMPPayload {
-    let events = get_gateway_events(tx);
-    let ProgramInvocationState::Succeeded(vec_events) = &events[0] else {
-        panic!("Expected successful program invocation");
-    };
-
-    let call_contract_event = vec_events
-        .iter()
-        .find(|(_, event)| {
-            matches!(
-                event,
-                axelar_solana_gateway::events::GatewayEvent::CallContract(_)
-            )
-        })
-        .expect("CallContract event not found");
-
-    let (_, axelar_solana_gateway::events::GatewayEvent::CallContract(event)) = call_contract_event
-    else {
-        panic!("Expected CallContract event");
-    };
+    let event = find_event_in_inner_instructions::<CallContractEvent>(inner_ixs)
+        .ok_or_else(|| anyhow!("CallContractEvent not found"))
+        .unwrap();
 
     let gmp_payload = GMPPayload::decode(&event.payload).unwrap();
     let GMPPayload::SendToHub(hub_message) = &gmp_payload else {
@@ -186,13 +169,25 @@ async fn test_memo_cpi_transfer(ctx: &mut ItsTestContext) {
     )
     .unwrap();
 
-    let tx = ctx.send_solana_tx(&[send_transfer]).await.unwrap();
+    // Simulate first to get the event
+    let simulation_result = ctx.simulate_solana_tx(&[send_transfer.clone()]).await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()
+        .first()
+        .cloned()
+        .unwrap();
 
     verify_gateway_event_and_source(
-        &tx,
+        &inner_ixs,
         &axelar_solana_memo_program::ID.to_bytes(),
         transfer_amount,
     );
+
+    // Then execute the transaction
+    ctx.send_solana_tx(&[send_transfer]).await.unwrap();
 }
 
 /// Test that CPI transfers fail when initiated by non-PDA accounts
@@ -340,13 +335,27 @@ async fn test_memo_cpi_call_contract_with_interchain_token(ctx: &mut ItsTestCont
         )
         .unwrap();
 
-    let tx = ctx.send_solana_tx(&[call_contract_transfer]).await.unwrap();
+    // Simulate first to get the event
+    let simulation_result = ctx
+        .simulate_solana_tx(&[call_contract_transfer.clone()])
+        .await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()
+        .first()
+        .cloned()
+        .unwrap();
 
     let gmp_payload = verify_gateway_event_and_source(
-        &tx,
+        &inner_ixs,
         &axelar_solana_memo_program::ID.to_bytes(),
         transfer_amount,
     );
+
+    // Then execute the transaction
+    ctx.send_solana_tx(&[call_contract_transfer]).await.unwrap();
 
     let GMPPayload::SendToHub(hub_message) = gmp_payload else {
         panic!("Expected SendToHub payload");

programs/axelar-solana-its/tests/module/token_id_validation.rs
@@ -14,7 +14,7 @@ use evm_contracts_test_suite::evm_contracts_rs::contracts::custom_test_token::Cu
 use evm_contracts_test_suite::ContractMiddleware;
 use interchain_token_transfer_gmp::GMPPayload;
 
-use crate::{fetch_first_call_contract_event_from_tx, ItsTestContext};
+use crate::{find_event_in_inner_instructions, ItsTestContext};
 
 /// Helper function to set up a custom token with a specific token manager type
 async fn setup_custom_token(
@@ -57,11 +57,25 @@ async fn setup_custom_token(
         0,
     )?;
 
-    let tx = ctx
-        .send_solana_tx(&[metadata_ix, register_metadata])
+    // Simulate first to get the event
+    let simulation_result = ctx
+        .simulate_solana_tx(&[metadata_ix.clone(), register_metadata.clone()])
+        .await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()[1]
+        .clone();
+    let call_contract_event = find_event_in_inner_instructions::<
+        axelar_solana_gateway::events::CallContractEvent,
+    >(&inner_ixs)
+    .expect("CallContractEvent not found");
+
+    // Then execute the transaction
+    ctx.send_solana_tx(&[metadata_ix, register_metadata])
         .await
         .unwrap();
-    let call_contract_event = fetch_first_call_contract_event_from_tx(&tx);
 
     let GMPPayload::RegisterTokenMetadata(register_message) =
         GMPPayload::decode(&call_contract_event.payload)?
@@ -107,8 +121,24 @@ async fn setup_custom_token(
         0,
     )?;
 
-    let tx = ctx.send_solana_tx(&[link_token_ix]).await.unwrap();
-    let call_contract_event = fetch_first_call_contract_event_from_tx(&tx);
+    // Simulate first to get the event
+    let simulation_result = ctx.simulate_solana_tx(&[link_token_ix.clone()]).await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()
+        .first()
+        .cloned()
+        .unwrap();
+    let call_contract_event = find_event_in_inner_instructions::<
+        axelar_solana_gateway::events::CallContractEvent,
+    >(&inner_ixs)
+    .expect("CallContractEvent not found");
+
+    // Then execute the transaction
+    ctx.send_solana_tx(&[link_token_ix]).await.unwrap();
+
     ctx.relay_to_evm(&call_contract_event.payload).await;
 
     Ok((token_id, custom_token, custom_solana_token))
@@ -195,11 +225,25 @@ async fn test_valid_token_id_mint_matches_token_address(
         0,
     )?;
 
+    // Simulate first to get the event
+    let simulation_result = ctx.simulate_solana_tx(&[transfer_ix.clone()]).await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()
+        .first()
+        .cloned()
+        .unwrap();
+    let call_contract_event = find_event_in_inner_instructions::<
+        axelar_solana_gateway::events::CallContractEvent,
+    >(&inner_ixs)
+    .expect("CallContractEvent not found");
+
     // This should succeed without errors
-    let tx = ctx.send_solana_tx(&[transfer_ix]).await.unwrap();
+    ctx.send_solana_tx(&[transfer_ix]).await.unwrap();
 
     // Verify the transfer event was emitted successfully
-    let call_contract_event = fetch_first_call_contract_event_from_tx(&tx);
     let GMPPayload::SendToHub(hub_message) = GMPPayload::decode(&call_contract_event.payload)?
     else {
         panic!("Expected SendToHub message");

axelarnetwork/axelar-contract-deployments

programs/axelar-solana-its/tests/module/deploy_remote.rs
@@ -1,10 +1,12 @@
-use event_utils::Event as _;
+use anyhow::anyhow;
 use mpl_token_metadata::accounts::Metadata;
 use mpl_token_metadata::instructions::CreateV1Builder;
 use mpl_token_metadata::types::TokenStandard;
 use solana_program_test::tokio;
 use solana_sdk::instruction::Instruction;
 use test_context::test_context;
+
+use event_cpi_test_utils::get_first_event_cpi_occurrence;
 
 use crate::ItsTestContext;
 
@@ -26,18 +28,24 @@ async fn test_deploy_remote_interchain_token_with_valid_metadata(
         Some(ctx.solana_wallet),
     )?;
 
-    let tx = ctx
-        .send_solana_tx(&[deploy_local_ix])
+    let simulation_result = ctx.simulate_solana_tx(&[deploy_local_ix.clone()]).await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()
+        .first()
+        .cloned()
+        .unwrap();
+    let deploy_event = get_first_event_cpi_occurrence::<
+        axelar_solana_its::events::InterchainTokenDeployed,
+    >(&inner_ixs)
+    .ok_or_else(|| anyhow!("InterchainTokenDeployed not found"))
+    .unwrap();
+
+    ctx.send_solana_tx(&[deploy_local_ix])
         .await
         .expect("InterchainToken deployment failed");
-
-    let deploy_event = tx
-        .metadata
-        .unwrap()
-        .log_messages
-        .iter()
-        .find_map(|log| axelar_solana_its::events::InterchainTokenDeployed::try_from_log(log).ok())
-        .unwrap();
 
     assert_eq!(
         deploy_event.name, "Valid Metadata Token",
@@ -71,6 +79,21 @@ async fn test_deploy_remote_interchain_token_with_valid_metadata(
             0,
         )?;
 
+    let simulation_result = ctx.simulate_solana_tx(&[deploy_remote_ix.clone()]).await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()
+        .first()
+        .cloned()
+        .unwrap();
+    let deployment_started_event = get_first_event_cpi_occurrence::<
+        axelar_solana_its::events::InterchainTokenDeploymentStarted,
+    >(&inner_ixs)
+    .ok_or_else(|| anyhow!("InterchainTokenDeploymentStarted not found"))
+    .unwrap();
+
     let tx = ctx.send_solana_tx(&[deploy_remote_ix]).await;
 
     // Transaction should succeed
@@ -78,17 +101,6 @@ async fn test_deploy_remote_interchain_token_with_valid_metadata(
         tx.is_ok(),
         "Expected deployment to succeed with valid metadata"
     );
-
-    let tx = tx.unwrap();
-    let deployment_started_event = tx
-        .metadata
-        .unwrap()
-        .log_messages
-        .iter()
-        .find_map(|log| {
-            axelar_solana_its::events::InterchainTokenDeploymentStarted::try_from_log(log).ok()
-        })
-        .unwrap();
 
     assert_eq!(
         deployment_started_event.token_name, "Valid Metadata Token",

programs/axelar-solana-its/tests/module/memo_cpi_transfer.rs
@@ -1,15 +1,16 @@
-use axelar_solana_gateway_test_fixtures::{
-    assert_msg_present_in_logs,
-    gateway::{get_gateway_events, ProgramInvocationState},
-};
+use anyhow::anyhow;
+use axelar_solana_gateway::events::CallContractEvent;
+use axelar_solana_gateway_test_fixtures::assert_msg_present_in_logs;
 use axelar_solana_its::state::token_manager::Type;
 use axelar_solana_memo_program::get_counter_pda;
 use evm_contracts_test_suite::ethers::signers::Signer as EvmSigner;
 use interchain_token_transfer_gmp::GMPPayload;
 use solana_program::pubkey::Pubkey;
-use solana_program_test::{tokio, BanksTransactionResultWithMetadata};
+use solana_program_test::tokio;
 use spl_associated_token_account::get_associated_token_address_with_program_id;
 use test_context::test_context;
+
+use event_cpi_test_utils::get_first_event_cpi_occurrence;
 
 use crate::{BorshPdaAccount, ItsTestContext};
 
@@ -107,31 +108,15 @@ async fn verify_token_manager_type(ctx: &mut ItsTestContext, token_manager_pda: 
     };
 }
 
-/// Extract and verify the CallContract event from a transaction
+/// Extract and verify the CallContract event from inner instructions
 fn verify_gateway_event_and_source(
-    tx: &BanksTransactionResultWithMetadata,
+    inner_ixs: &[solana_sdk::inner_instruction::InnerInstruction],
     expected_source: &[u8; 32],
     expected_amount: u64,
 ) -> GMPPayload {
-    let events = get_gateway_events(tx);
-    let ProgramInvocationState::Succeeded(vec_events) = &events[0] else {
-        panic!("Expected successful program invocation");
-    };
-
-    let call_contract_event = vec_events
-        .iter()
-        .find(|(_, event)| {
-            matches!(
-                event,
-                axelar_solana_gateway::events::GatewayEvent::CallContract(_)
-            )
-        })
-        .expect("CallContract event not found");
-
-    let (_, axelar_solana_gateway::events::GatewayEvent::CallContract(event)) = call_contract_event
-    else {
-        panic!("Expected CallContract event");
-    };
+    let event = get_first_event_cpi_occurrence::<CallContractEvent>(inner_ixs)
+        .ok_or_else(|| anyhow!("CallContractEvent not found"))
+        .unwrap();
 
     let gmp_payload = GMPPayload::decode(&event.payload).unwrap();
     let GMPPayload::SendToHub(hub_message) = &gmp_payload else {
@@ -186,13 +171,25 @@ async fn test_memo_cpi_transfer(ctx: &mut ItsTestContext) {
     )
     .unwrap();
 
-    let tx = ctx.send_solana_tx(&[send_transfer]).await.unwrap();
+    // Simulate first to get the event
+    let simulation_result = ctx.simulate_solana_tx(&[send_transfer.clone()]).await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()
+        .first()
+        .cloned()
+        .unwrap();
 
     verify_gateway_event_and_source(
-        &tx,
+        &inner_ixs,
         &axelar_solana_memo_program::ID.to_bytes(),
         transfer_amount,
     );
+
+    // Then execute the transaction
+    ctx.send_solana_tx(&[send_transfer]).await.unwrap();
 }
 
 /// Test that CPI transfers fail when initiated by non-PDA accounts
@@ -340,13 +337,27 @@ async fn test_memo_cpi_call_contract_with_interchain_token(ctx: &mut ItsTestCont
         )
         .unwrap();
 
-    let tx = ctx.send_solana_tx(&[call_contract_transfer]).await.unwrap();
+    // Simulate first to get the event
+    let simulation_result = ctx
+        .simulate_solana_tx(&[call_contract_transfer.clone()])
+        .await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()
+        .first()
+        .cloned()
+        .unwrap();
 
     let gmp_payload = verify_gateway_event_and_source(
-        &tx,
+        &inner_ixs,
         &axelar_solana_memo_program::ID.to_bytes(),
         transfer_amount,
     );
+
+    // Then execute the transaction
+    ctx.send_solana_tx(&[call_contract_transfer]).await.unwrap();
 
     let GMPPayload::SendToHub(hub_message) = gmp_payload else {
         panic!("Expected SendToHub payload");

programs/axelar-solana-its/tests/module/token_id_validation.rs
@@ -14,7 +14,9 @@ use evm_contracts_test_suite::evm_contracts_rs::contracts::custom_test_token::Cu
 use evm_contracts_test_suite::ContractMiddleware;
 use interchain_token_transfer_gmp::GMPPayload;
 
-use crate::{fetch_first_call_contract_event_from_tx, ItsTestContext};
+use event_cpi_test_utils::get_first_event_cpi_occurrence;
+
+use crate::ItsTestContext;
 
 /// Helper function to set up a custom token with a specific token manager type
 async fn setup_custom_token(
@@ -57,11 +59,25 @@ async fn setup_custom_token(
         0,
     )?;
 
-    let tx = ctx
-        .send_solana_tx(&[metadata_ix, register_metadata])
+    // Simulate first to get the event
+    let simulation_result = ctx
+        .simulate_solana_tx(&[metadata_ix.clone(), register_metadata.clone()])
+        .await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()[1]
+        .clone();
+    let call_contract_event = get_first_event_cpi_occurrence::<
+        axelar_solana_gateway::events::CallContractEvent,
+    >(&inner_ixs)
+    .expect("CallContractEvent not found");
+
+    // Then execute the transaction
+    ctx.send_solana_tx(&[metadata_ix, register_metadata])
         .await
         .unwrap();
-    let call_contract_event = fetch_first_call_contract_event_from_tx(&tx);
 
     let GMPPayload::RegisterTokenMetadata(register_message) =
         GMPPayload::decode(&call_contract_event.payload)?
@@ -107,8 +123,24 @@ async fn setup_custom_token(
         0,
     )?;
 
-    let tx = ctx.send_solana_tx(&[link_token_ix]).await.unwrap();
-    let call_contract_event = fetch_first_call_contract_event_from_tx(&tx);
+    // Simulate first to get the event
+    let simulation_result = ctx.simulate_solana_tx(&[link_token_ix.clone()]).await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()
+        .first()
+        .cloned()
+        .unwrap();
+    let call_contract_event = get_first_event_cpi_occurrence::<
+        axelar_solana_gateway::events::CallContractEvent,
+    >(&inner_ixs)
+    .expect("CallContractEvent not found");
+
+    // Then execute the transaction
+    ctx.send_solana_tx(&[link_token_ix]).await.unwrap();
+
     ctx.relay_to_evm(&call_contract_event.payload).await;
 
     Ok((token_id, custom_token, custom_solana_token))
@@ -195,11 +227,25 @@ async fn test_valid_token_id_mint_matches_token_address(
         0,
     )?;
 
+    // Simulate first to get the event
+    let simulation_result = ctx.simulate_solana_tx(&[transfer_ix.clone()]).await;
+    let inner_ixs = simulation_result
+        .simulation_details
+        .unwrap()
+        .inner_instructions
+        .unwrap()
+        .first()
+        .cloned()
+        .unwrap();
+    let call_contract_event = get_first_event_cpi_occurrence::<
+        axelar_solana_gateway::events::CallContractEvent,
+    >(&inner_ixs)
+    .expect("CallContractEvent not found");
+
     // This should succeed without errors
-    let tx = ctx.send_solana_tx(&[transfer_ix]).await.unwrap();
+    ctx.send_solana_tx(&[transfer_ix]).await.unwrap();
 
     // Verify the transfer event was emitted successfully
-    let call_contract_event = fetch_first_call_contract_event_from_tx(&tx);
     let GMPPayload::SendToHub(hub_message) = GMPPayload::decode(&call_contract_event.payload)?
     else {
         panic!("Expected SendToHub message");