289b33d3a0f8b7cec962e7e845292980fa451e96
isaac markus <isaac@interoplabs.io>
2025-10-19 15:45:54 -0400

    fix: remove extra debug print

--- evm/gateway.js
+++ evm/gateway.js
@@ -248,6 +248,7 @@ async function processCommand(axelar, chain, _chains, options) {
             const payloadHash = keccak256(arrayify(payload));
 
             const commandID = options.commandID.startsWith('0x') ? options.commandID : id(parseInt(options.commandID).toString());
+            printInfo('Command ID', commandID);
 
             if (await gateway.isCommandExecuted(commandID)) {
                 printWarn('Command already executed');
@@ -292,7 +293,6 @@ async function processCommand(axelar, chain, _chains, options) {
             if (options.messageId) {
                 // Derive commandID for Amplifier gateway
                 commandID = id(`${sourceChain}_${options.messageId}`);
-                printInfo('Command ID', commandID);
             } else {
                 commandID = options.commandID.startsWith('0x') ? options.commandID : id(parseInt(options.commandID).toString());
             }
