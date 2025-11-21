5a46666d7a21228a767c55f463b53891229ee7d1
Tomasz Kulik <tomek.kulik2@gmail.com>
2025-11-21 15:10:31 +0100

    refactor: reuse method & cleanup

--- cosmwasm/migrate/sdk50.ts
+++ cosmwasm/migrate/sdk50.ts
@@ -3,12 +3,10 @@
 import { StdFee } from '@cosmjs/stargate';
 import { Command } from 'commander';
 
-import { printInfo, prompt } from '../../common';
+import { getAmplifierChains, printInfo, prompt } from '../../common';
 import { ConfigManager } from '../../common/config';
 import { addAmplifierOptions } from '../cli-utils';
-import { ClientManager } from '../processor';
-import { mainProcessor } from '../processor';
-import { Options } from '../processor';
+import { ClientManager, Options, mainProcessor } from '../processor';
 import { encodeExecuteContract, encodeMigrate, getCodeId, submitProposal } from '../utils';
 
 interface MigrationOptions extends Options {
@@ -28,21 +26,18 @@ async function migrateAllVotingVerifiers(
     _args: string[],
     fee: string | StdFee,
 ): Promise<void> {
-    const { fetchCodeId } = options;
-    const chains = Object.entries(config.chains)
-        .filter(([, chainConfig]) => chainConfig.contracts?.AxelarGateway?.connectionType === 'amplifier')
-        .map(([chainName]) => chainName);
-    const votingVerifiers: Array<{ chainName: string; address: string; codeId: number }> = [];
+    const chains = getAmplifierChains(config);
+    const votingVerifiers: Array<{ chainName: string; address: string; codeId: number; contractName: string }> = [];
     options.title = options.title || 'Migrate Voting Verifiers to update block time related parameters';
     options.description = options.description || 'Migrate all voting verifiers to update block time related parameters';
 
-    for (const chainName of chains) {
-        const chainConfig = config.getChainConfig(chainName);
+    for (const { name: chainName, config: chainConfig } of chains) {
         const votingVerifierConfig = config.getVotingVerifierContract(chainName);
+        const contractName = config.getVotingVerifierContractForChainType(chainConfig.chainType);
         config.validateRequired(votingVerifierConfig.address, 'votingVerifierConfig.address');
         const codeId = await getCodeId(client, config, {
             ...options,
-            contractName: config.getVotingVerifierContractForChainType(chainConfig.chainType),
+            contractName,
         });
 
         votingVerifierConfig.codeId = codeId;
@@ -51,19 +46,19 @@ async function migrateAllVotingVerifiers(
             chainName,
             address: votingVerifierConfig.address,
             codeId,
+            contractName,
         });
         printInfo(`Added ${chainName} voting verifier (address: ${votingVerifierConfig.address}, codeId: ${codeId})`);
     }
 
     printInfo(`Found ${votingVerifiers.length} voting verifier(s) to migrate`);
 
-    const migrationMessages = votingVerifiers.map(({ chainName, address, codeId }) => {
-        const chainConfig = config.getChainConfig(chainName);
+    const migrationMessages = votingVerifiers.map(({ chainName, address, codeId, contractName }) => {
         return {
             chainName,
             message: encodeMigrate(config, {
                 ...options,
-                contractName: config.getVotingVerifierContractForChainType(chainConfig.chainType),
+                contractName,
                 chainName,
                 address,
                 codeId,
@@ -90,19 +85,16 @@ async function updateBlockTimeRelatedParameters(
     _args: string[],
     fee: string | StdFee,
 ): Promise<void> {
-    const chains = Object.entries(config.chains)
-        .filter(([, chainConfig]) => chainConfig.contracts?.AxelarGateway?.connectionType === 'amplifier')
-        .map(([chainName]) => chainName);
+    const chains = getAmplifierChains(config);
     options.title = options.title || 'Update block time related parameters for all voting verifiers';
     options.description = options.description || 'Update block time related parameters for all voting verifiers';
 
     const votingVerifierMessages = await Promise.all(
-        chains.map(async (chainName) => {
-            const chainConfig = config.getChainConfig(chainName);
+        chains.map(async ({ name: chainName, config: chainConfig }) => {
             const votingVerifierConfig = config.getVotingVerifierContract(chainName);
             config.validateRequired(votingVerifierConfig.address, 'votingVerifierConfig.address');
 
-            const { block_expiry, confirmation_height } = await client.queryContractSmart(
+            const { block_expiry, confirmation_height, voting_threshold } = await client.queryContractSmart(
                 votingVerifierConfig.address,
                 'voting_parameters',
             );
@@ -111,6 +103,7 @@ async function updateBlockTimeRelatedParameters(
                 update_voting_parameters: {
                     block_expiry: votingVerifierConfig.blockExpiry,
                     confirmation_height: votingVerifierConfig.confirmationHeight,
+                    voting_threshold: null,
                 },
             };
             printInfo(
@@ -151,8 +144,8 @@ async function updateSigningParametersForMultisig(
     fee: string | StdFee,
 ): Promise<void> {
     const multisigConfig = config.getContractConfig('Multisig');
-    config.validateRequired(multisigConfig.address, 'multisigConfig.address');
-    config.validateRequired(multisigConfig.blockExpiry, 'multisigConfig.blockExpiry', 'number');
+    config.validateRequired(multisigConfig.address, 'axelar.contracts.Multisig.address', 'string');
+    config.validateRequired(multisigConfig.blockExpiry, 'axelar.contracts.Multisig.blockExpiry', 'number');
     options.title = options.title || 'Update signing parameters for multisig';
     options.description = options.description || 'Update signing parameters for multisig';
 
