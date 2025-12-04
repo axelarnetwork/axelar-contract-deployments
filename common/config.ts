3169278aa57a759f3590e32a96654e0ce3b8db85
benjamin852 <weinbergben33@gmail.com>
2025-12-04 14:56:24 -0500

    chore: remove unnecessary getAdmin helper

--- common/config.ts
+++ common/config.ts
@@ -363,10 +363,6 @@ export class ConfigManager implements FullConfig {
         return this.axelar.govProposalDepositAmount;
     }
 
-    public getAdmin(): string | undefined {
-        return this.axelar.adminAddress;
-    }
-
     public getChainConfig(chainName: string): ChainConfig {
         const chainConfig = this.chains[chainName];
         if (!chainConfig) {
