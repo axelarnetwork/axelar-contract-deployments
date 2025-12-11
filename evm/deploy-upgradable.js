f9f20892abcee4080e4c3ec67be1945743beb318
blockchainguyy <ayusht11@outlook.com>
2025-12-11 16:19:48 +0530

    fix: prompt message

--- evm/deploy-upgradable.js
+++ evm/deploy-upgradable.js
@@ -235,13 +235,7 @@ async function processCommand(_axelar, chain, _chains, options) {
         printInfo('Deployer contract', deployerContract);
         printInfo(`${contractName} implementation will be deployed to`, predictedImplementationAddress, chalk.cyan);
 
-        if (
-            predictOnly ||
-            prompt(
-                `Does derived implementation address match existing deployments? Proceed with implementation deployment on ${chain.name}?`,
-                yes,
-            )
-        ) {
+        if (predictOnly || prompt(`Proceed with new implementation deployment on ${chain.name}?`, yes)) {
             return;
         }
 
