109f8bbca09c258a8974cf30fbc3804258059f63
benjamin852 <weinbergben33@gmail.com>
2025-12-11 18:16:14 -0500

    fix: resolve merge conflict in governance.js

--- evm/governance.js
+++ evm/governance.js
@@ -187,16 +187,11 @@ function encodeGovernanceProposal(commandType, target, calldata, nativeValue, et
     return defaultAbiCoder.encode(types, values);
 }
 
-<<<<<<< HEAD
 function getProposalHash(target, calldata, nativeValue) {
     return keccak256(defaultAbiCoder.encode(['address', 'bytes', 'uint256'], [target, calldata, nativeValue]));
 }
-=======
-async function processCommand(axelar, chain, _chains, options) {
-    const { contractName, address, action, proposalAction, date, privateKey, yes } = options;
->>>>>>> 3c7194a9 (chore: added multisigproveradmin address)
 
-async function processCommand(_axelar, chain, _chains, action, options) {
+async function processCommand(axelar, chain, _chains, options) {
     const { contractName, address, privateKey, args = [] } = options;
 
     const governanceAddress = getGovernanceAddress(chain, contractName, address);
