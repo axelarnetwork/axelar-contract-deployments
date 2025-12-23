53042d8cbbf2c5792f38a3450e876c2b448318fe
SDavidson1177 <solomon@interoplabs.io>
2025-12-23 17:08:14 -0500

    add rpc argument to rotation

--- sui/utils/cli-utils.js
+++ sui/utils/cli-utils.js
@@ -44,6 +44,10 @@ const addBaseOptions = (program, options = {}) => {
         program.addOption(new Option('--txFilePath <file>', 'unsigned transaction will be stored'));
     }
 
+    if (options.allowCustomRpc) {
+        program.addOption(new Option('--rpc <rpc>', 'Sui rpc endpoint'));
+    }
+
     return program;
 };
 
